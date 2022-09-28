43c246692957b44cb1f601ade8a3c6091eafd5e4
sikorsj <jan.sikorski@play.pl>
2022-09-28 12:09:58 +0200

    Added Python solution for finding volume of a cuboid

---
+++ program/find-volume-of-a-cuboid/find-volume-of-a-cuboid.py
@@ -0,0 +1,6 @@
+def find_volume_of_a_cuboid(length, width, height):
+    return length * width * height
+
+if __name__ == "__main__":
+    print(find_volume_of_a_cuboid(1, 2, 3))
+    print(find_volume_of_a_cuboid(1.1, 2.2, 3.3))
