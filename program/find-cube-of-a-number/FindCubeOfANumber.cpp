97d793bd32c9a5412b769c5c92117eb26fbd52d2
GAUTAM NAHAR <nahargautam001@gmail.com>
2022-09-29 23:03:09 +0530

    Adding a c++ program to find cube of a number

---
+++ program/find-cube-of-a-number/FindCubeOfANumber.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num; // declaring an integer variable num
+    cin>>num; // taking input from user and storing it in num
+    cout<<num*num*num; // printing cube of the number
+}
