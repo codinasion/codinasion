62e17729b327c00594010dc61c6ae7dffa571acd
Harsh Raj <harshraj8843@gmail.com>
2022-09-29 21:18:32 +0530

    Update ConvertMetersToMiles.ts
    Co-authored-by: Muhammad Adil <madil89@gmail.com>

--- program/convert-meters-to-miles/ConvertMetersToMiles.ts
+++ program/convert-meters-to-miles/ConvertMetersToMiles.ts
@@ -1,1 +1,5 @@
-export const convertMetersToMiles = (meters:number):number =>  meters / 1609.344;
+export const convertMetersToMiles = (meters: number): number => meters / 1609.344;
+
+const meters = 8046.72;
+console.log("Input  : " + meters);
+console.log("Output : " + convertMetersToMiles(meters));
