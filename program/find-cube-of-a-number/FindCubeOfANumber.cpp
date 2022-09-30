237887d15fa0057ebf2960663283a3dc2497a46a
Harsh Raj <harshraj8843@gmail.com>
2022-09-30 07:29:54 +0530

    Update FindCubeOfANumber.cpp
    Co-authored-by: GAUTAM NAHAR <nahargautam001@gmail.com>

--- program/find-cube-of-a-number/FindCubeOfANumber.cpp
+++ program/find-cube-of-a-number/FindCubeOfANumber.cpp
@@ -1,8 +1,11 @@
-#include<iostream>
+#include <iostream>
+
 using namespace std;
 
-int main(){
+int main()
+{
     int num; // declaring an integer variable num
-    cin>>num; // taking input from user and storing it in num
-    cout<<num*num*num; // printing cube of the number
+    cout << "Input  : ";
+    cin >> num;                                     // taking input from user and storing it in num
+    cout << "Output : " << num * num * num << endl; // printing cube of the number
 }
