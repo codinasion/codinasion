533b25db50e35c7645383a6d9ae9b931b19444f4
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2022-09-30 21:15:52 +0100

    Created a JS programme for DivideTwoNumbers.
    * Used a couple of use-cases.
    * Handled the edge case scenarios.
    * Documented both Testing on Browser and Console Tests.

---
+++ program/divide-two-numbers/DivideTwoNumbers.js
@@ -0,0 +1,21 @@
+// Program to divide two numbers using JavaScript
+function divideTwoNumbers(num1, num2) {
+  if (num2 === 0) {
+    console.error("Divide by Zero Error!");
+    return null;
+  }
+  return num1 / num2;
+}
+
+// Testing on Browser
+// const n1 = prompt("Num 1: ");
+// const n2 = prompt("Num 2: ");
+// alert(divideTwoNumbers(n1, n2));
+
+// Console Tests
+console.log("Normal Case");
+console.log(`Input: n1 = ${n1} and n2 = ${n2}`);
+console.log(`Output: ${divideTwoNumbers(n1, n2)}`);
+console.log("Edge Case");
+console.log(`Input: n1 = ${n1} and n2 = 0`);
+console.log(`Output: ${divideTwoNumbers(n1, 0)}`);
