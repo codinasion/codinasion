793b93f362ce7bc35a26f2e001ccb8242003975d
Praveen Kumar Purushothaman <praveenscience@gmail.com>
2022-10-01 23:06:18 +0100

    Created divide_two_numbers function in PHP.
    * Takes two numbers as input.
    * Handles null values as input.
    * Handles division by zero error.
    * Handles numeric check of numbers.
    
    Closes #117.

---
+++ program/divide-two-numbers/divide_two_numbers.php
@@ -0,0 +1,13 @@
+<?php
+	// Program to divide two numbers.
+	function divide_two_numbers($num1, $num2 = 1) {
+		if (!is_numeric($num1) || !is_numeric($num2) || $num2 == 0) {
+			return null;
+		}
+		return $num1 / $num2;
+	}
+	// Tests
+	print_r(divide_two_numbers(2, 4));        // 1
+	print_r(divide_two_numbers(15, "5"));     // 2.66
+	print_r(divide_two_numbers("600", 0));    // null or empty
+	print_r(divide_two_numbers("Hello"));     // null or empty
