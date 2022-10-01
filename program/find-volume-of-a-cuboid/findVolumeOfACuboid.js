5082a931f91376ff0831f810223da11269a12fdb
amitanshusahu <amitansusahu@gmail.com>
2022-10-02 01:57:57 +0530

    volume of cuboid found in js

---
+++ program/find-volume-of-a-cuboid/findVolumeOfACuboid.js
@@ -0,0 +1,11 @@
+//program to find the volume of a cubeoid
+
+function volumeOfCuboid(length , breath , height){
+    return length*breath*height
+}
+
+let length = 12;
+let breath = 20;
+let height = 5; 
+
+volumeOfCuboid(length, breath, height);
