b31626411c1bb60a9ecbc95b638a395f28914b2a
Harsh Raj <harshraj8843@gmail.com>
2022-10-01 12:06:55 +0530

    Update FindVolumeOfACuboid.c

--- program/find-volume-of-a-cuboid/FindVolumeOfACuboid.c
+++ program/find-volume-of-a-cuboid/FindVolumeOfACuboid.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
+
 void main()
 {
     int l, w, h, volume;
     printf("Enter value of length : ");
     scanf("%d", &l);
-    printf("Enter value of width : ");
+    printf("Enter value of width  : ");
     scanf("%d", &w);
     printf("Enter value of height : ");
     scanf("%d", &h);
     volume = l * w * h;
 
-    printf("volume of cuboid is : %d", volume);
+    printf("\nVolume of cuboid is : %d\n", volume);
 }
