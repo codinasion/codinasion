7d4ba9f94660c07e793b23010f8ba69b86c13e52
rwlc <83243777+rwlc@users.noreply.github.com>
2022-10-02 06:41:26 +0000

    refactor: format code

--- program/print-reverse-of-a-string-by-recursion/PrintReverseOfAStringByRecursion.ts
+++ program/print-reverse-of-a-string-by-recursion/PrintReverseOfAStringByRecursion.ts
@@ -1,10 +1,7 @@
 type ReversedString<T> = T extends `${infer U}${infer R}` ? `${ReversedString<R>}${U}` : T
 
 function printReverseOfAStringByRecursion(str: string): string {
-
-
-    if(!str.length) return ""
-
+    if (!str.length) return ""
     return printReverseOfAStringByRecursion(str.substring(1)) + str[0]
 }
 
