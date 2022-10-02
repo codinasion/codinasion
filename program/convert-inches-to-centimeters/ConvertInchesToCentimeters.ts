470d47d4e2d7a2930251dfa74d05f676356f75c2
MaestroDagan <ditisdagan@hotmail.com>
2022-10-02 18:12:44 +0200

    feat: made it a function

--- program/convert-inches-to-centimeters/ConvertInchesToCentimeters.ts
+++ program/convert-inches-to-centimeters/ConvertInchesToCentimeters.ts
@@ -1,3 +1,3 @@
-const inches: number = 5;
-
-console.log(`${inches} inch(es) = ${inches * 2.54} centimeter(s)`);
+export function ConvertInchesToCentimeters(inches: number) {
+  console.log(`${inches} inch(es) = ${inches * 2.54} centimeter(s)`);
+};
