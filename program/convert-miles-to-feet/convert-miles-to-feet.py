85e918649a2cbdfe9aba41e3256f45245dc5700f
Eniola Ademola <it.eniolaademola@gmail.com>
2022-10-02 01:23:07 +0100

    add python code to convert mile to feet

---
+++ program/convert-miles-to-feet/convert-miles-to-feet.py
@@ -0,0 +1,13 @@
+print(
+    "JavaScript program to convert miles to feet \n 1 mile = 5280 feet"
+)
+
+
+def milesToFeet(num):
+    return num * 5280
+
+
+miles = 10
+feet = milesToFeet(miles)
+print(f"Input  : {miles}")
+print(f"Output : {feet}")
