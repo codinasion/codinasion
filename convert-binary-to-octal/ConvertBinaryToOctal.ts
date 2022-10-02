66eca520ca728f401a0277bed402cb436bb5d8cf
Harsh Raj <harshraj8843@gmail.com>
2022-10-02 23:25:11 +0530

    Delete convert-binary-to-octal directory

--- convert-binary-to-octal/ConvertBinaryToOctal.ts
+++
@@ -1,8 +0,0 @@
-const binaryToDecimal = (input: number): number => {
-  return parseInt(input.toString(), 2);
-};
-
-export const binaryToOctal = (input: number): number => {
-  const decimal = binaryToDecimal(input);
-  return Number(decimal.toString(8));
-};
