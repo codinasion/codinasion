29d2cafc9f685426cfb0c050b70e26e9a81c9ae7
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-10-02 16:22:54 +0100

    divide two numbers C# completed

---
+++ program/divide-two-numbers/divide-two-numbers.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Sample {
+   class Demo {
+      static void Main(string[] args) {
+         int num1 = 50;
+         int num2 = 25;
+         int result;
+         result = num1 / num2;
+         Console.WriteLine("Division: Value is {0}", result) 
+      }
+   }
+}
