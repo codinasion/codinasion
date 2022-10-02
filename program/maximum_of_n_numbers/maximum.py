b8cbfaa09ee43fa5c03c05aa25401274961b36b1
Rachit <rachitgarg146@gmail.com>
2022-10-02 23:02:40 +0530

    maximum of n numbers code added

---
+++ program/maximum_of_n_numbers/maximum.py
@@ -0,0 +1,17 @@
+def find_max( list ):
+    max = list[ 0 ]
+    for a in list:
+        if a > max:
+            max = a
+    return max
+ 
+ 
+num = int(input('How many numbers: '))
+ 
+lst = []
+ 
+for n in range(num):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+     
+print("Maximum element in the list is :", find_max(lst))
