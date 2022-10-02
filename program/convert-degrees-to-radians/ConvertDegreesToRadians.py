514a189f39798afb4224c70cc63195155fe912dd
xemeds <xemeds@protonmail.com>
2022-10-02 22:43:01 +0300

    Added Python program to convert degrees to radians.

---
+++ program/convert-degrees-to-radians/ConvertDegreesToRadians.py
@@ -0,0 +1,13 @@
+from math import pi
+
+print("Python program to convert degrees to radians \n 180 degrees = π radians")
+
+def degreesToRadians(degree):
+    return (degree * pi / 180)
+
+
+degrees = 180
+radians = degreesToRadians(feet)
+
+print(f"Input  : {degrees}")
+print(f"Output : {radians}")
