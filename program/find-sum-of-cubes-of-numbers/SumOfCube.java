321377194770ea913b85c77c4ff070e431ef1cbe
raj <agentR47@gmail.com>
2022-10-03 13:57:34 +0530

    feat: Add sum-of-cube solution in java, c, cpp

---
+++ program/find-sum-of-cubes-of-numbers/SumOfCube.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class SumOfCube {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += Math.pow(arr[i], 3);
+        }
+        System.out.println(sum);
+    }
+
+}
