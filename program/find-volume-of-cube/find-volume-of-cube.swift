10e6e15abfa2c4f0a018677e682a5c1045dd0afd
areesha <areeshasayed09@gmail.com>
2022-10-03 07:52:17 +0530

    find-volume-of-cube-swift(codinsion#81)

--- program/find-volume-of-cube/find-volume-of-cube.swift
+++ program/find-volume-of-cube/find-volume-of-cube.swift
@@ -3,5 +3,5 @@ var cubeSide : Int = 2
 // Finding the volume of the cube 
 var cubeVolume = cubeSide * cubeSide * cubeSide
 
-print("Side - ", cubeSide) 
-print("So, Volume of the cube- ", cubeVolume)
+print("Side = ", cubeSide) 
+print("So, Volume of the cube= ", cubeVolume)
