ebfb88a244bd677a3ccc51fced5c15a54aa345b6
Sidharth Sarangi <78564629+SidharthSarangi@users.noreply.github.com>
2022-10-03 22:45:53 +0530

    Create FindSquareOfANumber.cpp

---
+++ program/find-square-of-a-number/FindSquareOfANumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n  ;
+    cout<<"Enter a no. : "<<endl; 
+    cin>>n ;
+    
+    cout<<"Square of "<<n<<" is "<<pow(n,2) ;
+
+    return 0;
+}
