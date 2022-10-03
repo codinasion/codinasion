9054f4f43bcb2ed18de5f1cc70d869f072cd6e6c
Harsh Raj <harshraj8843@gmail.com>
2022-10-04 02:10:47 +0530

    Delete find-volume-of-cube.swift

--- program/find-volume-of-a-cuboid/find-volume-of-cube.swift
+++
@@ -1,7 +0,0 @@
-var cubeSide : Int = 2
-
-// Finding the volume of the cube 
-var cubeVolume = cubeSide * cubeSide * cubeSide
-
-print("Side - ", cubeSide) 
-print("So, Volume of the cube- ", cubeVolume)
