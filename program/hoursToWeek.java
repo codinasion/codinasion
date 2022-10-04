5234c17b1790b356f7edcd733122b41571313b05
yashgoel2211 <114853033+yashgoel2211@users.noreply.github.com>
2022-10-04 17:09:21 +0530

    Add files via upload

---
+++ program/hoursToWeek.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main 
+{
+   public static void main(String args[])
+   {
+        Scanner sc=new Scanner(System.in);
+
+        double hour = 450;
+        double week = 3.5;
+        double wk = hour / 168;
+        System.out.println("Value of "+hour+" hour in week: "+ wk);  
+   }
+}
