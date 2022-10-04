516f35a5c1617af048fbc51dc9cea0943f946835
areesha <areeshasayed09@gmail.com>
2022-10-04 13:48:32 +0530

    convert-binary-to-hexadecimal (codinasion #936)

--- program/convert-binary-to-hexadecimal/ConvertBinaryToHexadecimal.java
+++ program/convert-binary-to-hexadecimal/ConvertBinaryToHexadecimal.java
@@ -2,8 +2,8 @@ import java.lang.*;
 import java.util.*;
 public class ConvertBinaryToHexadecimal{  
     public static void main(String args[]){  
-    Scanner sc = new Scanner(System.in);
-    String binaryString=sc.nextLine();  
+    Scanner scn = new Scanner(System.in);
+    String binaryString=scn.nextLine();  
     int decimal=Integer.parseInt(binaryString,2);  
     System.out.println(Integer.toHexString(decimal).toUpperCase());  
     }
