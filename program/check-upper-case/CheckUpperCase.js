446db221a05c06406831a4cb71fbc94266cca65a
rwlc <83243777+rwlc@users.noreply.github.com>
2022-10-05 03:28:16 +0530

    Update CheckUpperCase.js

--- program/check-upper-case/CheckUpperCase.js
+++ program/check-upper-case/CheckUpperCase.js
@@ -3,7 +3,7 @@
  * @param {string} character 
  * @returns {boolean} whether `character` is uppercase or nott
  */
-function checkUpperCaseInTS(character) {
+function checkUpperCase(character) {
     /**
      * Better implenetation than converting the character toUpperCase and checking. 
      */
