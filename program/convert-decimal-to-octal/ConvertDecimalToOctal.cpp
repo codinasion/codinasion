1eb662dd05c779c8b0a9fb6d0a4a426762a3b5e8
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2022-10-04 17:29:14 +0530

    changes

--- program/convert-decimal-to-octal/ConvertDecimalToOctal.cpp
+++ program/convert-decimal-to-octal/ConvertDecimalToOctal.cpp
@@ -3,11 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-<<<<<<< HEAD
-string decimal_to_octal(int n){
-=======
 string decimaltooctal(int n){
->>>>>>> 1829dbeed4b3afcd80e50776837801f9fcdd1103
     string ans="";
     for(int i=n;i>0;i=i/8){
         int mod=i%8;
@@ -22,9 +18,5 @@ int main(){
     int n;
     cout<<"Enter a number"<<'\n';
     cin>>n;
-<<<<<<< HEAD
-    cout<<decimal_to_octal(n)<<'\n';
-=======
     cout<<decimaltooctal(n)<<'\n';
->>>>>>> 1829dbeed4b3afcd80e50776837801f9fcdd1103
 }
