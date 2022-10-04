27113e5a9730229715cdd5220986c88fb5018bad
deepu13 <deepu13october@gmail.com>
2022-10-05 01:35:33 +0530

    added check_vowel.php

---
+++ program/check-vowel/check_vowel.php
@@ -0,0 +1,11 @@
+<?php
+
+$ch='a';
+
+function check_vowel($ch) 
+{ 
+if ($ch == 'a' || $ch == 'e' ||  $ch == 'i' || $ch == 'o' ||  $ch == 'u' || $ch == 'A' || $ch == 'E' ||  $ch == 'I' || $ch == 'O' ||  $ch == 'U') 
+  echo "Vowel" ; 
+} 
+
+?>
