b5d3b9485a7536820a6ee99b92a3890fa8ffaf9e
Amanjeet Kumar <amanjeet.kumar@cogoport.com>
2022-10-04 15:38:24 +0530

    done with this program

---
+++ program/convert-weeks-to-hours/convert-weeks-to-hours.jl
@@ -0,0 +1,14 @@
+result = 0
+  
+# Prompt to enter
+total_hrs = 0
+
+println("Enter number of weeks : ")
+
+num_of_weeks = readline()
+num_of_weeks = parse(Int64, num) 
+  
+total_hrs = num_of_weeks * 168
+
+  
+println("Total hrs :", total_hrs)
