2bfee8d73832720393c45eccf485c4c805f7e2e3
Chaitanya Sheth <97539791+Chaitanya-2804@users.noreply.github.com>
2022-10-05 09:48:00 +0530

    Added surface area code in C++

---
+++ SurfaceArea.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int side;
+    cout<<"Enter the side length of a cube: ";
+    cin>>side;
+    cout<<"The surface area of the cube is "<<6*side*side<<endl;
+
+
+    return 0;
+}
