0e80bde5b4485b53ac72beb29d64144e39f05937
Александр Сдобников <sdobnikove@mail.ru>
2022-10-05 13:59:08 +0300

    add CheckPalindromeNumber.ts #2001

---
+++ program/check-palindrome-number/CheckPalindromeNumber.ts
@@ -0,0 +1,32 @@
+export default (num: number):string => {
+    const length = num.toString().length;
+    const digits = num.toString().split('');
+    const realDigits = digits.map(Number);
+    if (length % 2 === 0) {
+        const firstPart = realDigits
+            .filter((val, index) => index < realDigits.length / 2);
+        const secondPartReversed = realDigits
+            .filter((val, index) => index >= realDigits.length / 2)
+            .reverse();
+        const firstPartNumber = Number(firstPart
+            .map((val) => val.toString())
+            .join(''));
+        const secondPartReversedNumber = Number(secondPartReversed
+            .map((val) => val.toString())
+            .join(''));
+        return firstPartNumber === secondPartReversedNumber ? 'Palindrome Number' : 'Not Palindrome Number';
+    } else {
+        const firstPart = realDigits
+            .filter((val, index) => index < Math.ceil(realDigits.length / 2));
+        const secondPartReversed = realDigits
+            .filter((val, index) => index >= Math.ceil(realDigits.length / 2 - 1))
+            .reverse();
+        const firstPartNumber = Number(firstPart
+            .map((val) => val.toString())
+            .join(''));
+        const secondPartReversedNumber = Number(secondPartReversed
+            .map((val) => val.toString())
+            .join(''));
+        return firstPartNumber === secondPartReversedNumber ? 'Palindrome Number' : 'Not Palindrome Number';
+    }
+};
