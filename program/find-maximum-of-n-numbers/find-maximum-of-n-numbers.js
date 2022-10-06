bb1fdf506c34188bff225b5b9205daf1f7047651
Zeuhz-Droid <davidadegbiran2020@gmail.com>
2022-10-06 01:42:48 +0100

    removed JS file

--- program/find-maximum-of-n-numbers/find-maximum-of-n-numbers.js
+++
@@ -1,9 +0,0 @@
-const getMaximum = (...nums) => {
-  let arr = [...nums];
-  let max = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > max) max = arr[i];
-  }
-
-  return max;
-};
