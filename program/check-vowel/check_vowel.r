053b8bd733f4170cc18bcb20f249ac1e967f717b
Yash Bajaj <bajajy82@gmail.com>
2022-10-06 23:15:14 +0530

    fixed added check for uppercase characters

--- program/check-vowel/check_vowel.r
+++ program/check-vowel/check_vowel.r
@@ -1,10 +1,14 @@
 checkVowel = function(char){
-    if(char %in% c('a', 'e', 'i', 'o', 'u'))
+    if(char %in% c('a', 'e', 'i', 'o', 'u','A','E','I','O','U'))
         print("Vowel")
     else 
         print("Consonant")
 }
 
 checkVowel('a')
+# Output
+# Vowel
+
+checkVowel('E')
 # Output
 # Vowel
