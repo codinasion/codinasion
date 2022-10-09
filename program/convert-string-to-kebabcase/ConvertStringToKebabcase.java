c11b055f9855d6a2a5139fce72efbcd58ce2bb2c
Hardik Gupta <113587014+CodingWithHardik@users.noreply.github.com>
2022-10-09 15:02:13 +0530

    added input line

--- program/convert-string-to-kebabcase/ConvertStringToKebabcase.java
+++ program/convert-string-to-kebabcase/ConvertStringToKebabcase.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
+	    System.out.println("Input  : ");
 	    Scanner sc=new Scanner(System.in);
 	    String a=sc.nextLine();
 	    String b=a.replace(' ','-');
