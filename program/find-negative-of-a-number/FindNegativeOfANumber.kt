90c8dae2f7dfb5be937a29decda5fde4ef0f2806
Debarpan Jha <jhadebarpan@gmail.com>
2022-10-09 13:45:38 +0530

    solves issue no #1821

---
+++ program/find-negative-of-a-number/FindNegativeOfANumber.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+
+    val number = 5
+     if (number > 0.0)
+        println("-"+"$number")
+    else  if (number < 0.0)
+        println("$number")
+    else
+        println("You have entered 0.")
+}
+//Contributed By Debarpan Jha
