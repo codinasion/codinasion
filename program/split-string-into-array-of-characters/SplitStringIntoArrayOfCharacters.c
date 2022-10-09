f6289e9cda2fff9d30d0fae4089ea717b4b5fa91
pravunath <pravunathsingh@gmail.com>
2022-10-09 21:47:13 +0530

    C program to split a string into a character array

---
+++ program/split-string-into-array-of-characters/SplitStringIntoArrayOfCharacters.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main()
+{
+  size_t arrayLength = 0, allocatedSize = 8;
+  int *array = malloc(sizeof(*array) * allocatedSize);
+  char str[1024];
+  fgets(str, 1024, stdin);
+  for (int i = 0; i < strlen(str); ++i)
+  {
+    if (arrayLength == allocatedSize)
+    {
+      array = realloc(array, allocatedSize *= 2);
+      assert(array);
+    }
+    assert(arrayLength < allocatedSize);
+    array[arrayLength++] = str[i];
+  }
+
+  for (int i = 0; i < arrayLength; ++i)
+    printf("%c ", array[i]);
+  free(array);
+
+  return 0;
+}
