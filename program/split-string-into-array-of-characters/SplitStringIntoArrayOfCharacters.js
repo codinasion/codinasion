8cc9e4cbb70283fd77f5a52b99eee35f121c13c0
Greeshma <100756636+Greeshma-frontier@users.noreply.github.com>
2022-10-09 20:01:22 +0530

    Update SplitStringIntoArrayOfCharacters.js

--- program/split-string-into-array-of-characters/SplitStringIntoArrayOfCharacters.js
+++ program/split-string-into-array-of-characters/SplitStringIntoArrayOfCharacters.js
@@ -1,5 +1,15 @@
 function splitStringIntoArrayOfCharacters(str) {
+
+    // Method 1
     let charArray = str.split('');
+    console.log(charArray);
+    
+    
+    // Method 2
+    let charArray = [];
+    for (let i = 0; i < str.length; i++) {
+        charArray.push(str[i]);
+    }
     console.log(charArray);
 }
 
