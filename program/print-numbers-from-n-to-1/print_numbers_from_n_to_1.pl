4339635c00e13e827a56b7b7e4fa4e5538640a84
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-10-10 12:39:30 +0530

    done with this eg. in kotlin

---
+++ program/print-numbers-from-n-to-1/print_numbers_from_n_to_1.pl
@@ -0,0 +1,14 @@
+# Write a Perl program to print numbers from n to 1
+
+# Input  : 5
+# Output : 5 4 3 2 1
+
+$num = 5;
+
+$i = num
+do {
+   print "$i ";
+   $i -= 1;
+} while( $i > 0 );
+
+# Contributed by Technical Amanjeet 
