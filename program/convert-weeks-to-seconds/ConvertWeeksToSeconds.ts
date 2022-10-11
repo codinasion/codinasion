1fe8828bd778ac58fee589e0b21b971ef22aff70
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-10-12 00:33:14 +0530

    done with this program in typescript

---
+++ program/convert-weeks-to-seconds/ConvertWeeksToSeconds.ts
@@ -0,0 +1,18 @@
+
+
+// g
+
+function convertWeeksToSeconds(weeks: number) 
+{
+  let seconds =  weeks*7*24*60*60*60
+
+  return convertWeeksToSeconds
+}
+
+
+let weeks = 5
+let seconds = convertWeeksToSeconds(weeks)
+
+console.log(seconds)
+
+// contributed by technical Amanjeet
