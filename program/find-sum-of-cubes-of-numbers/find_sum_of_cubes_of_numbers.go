98f3dd7adc3e1fecc9638a8b51f8e11a2a27aab9
Madhu S Gowda <79962713+MadhuS-1605@users.noreply.github.com>
2022-10-11 11:41:25 +0530

    Create find_sum_of_cubes_of_numbers.go

---
+++ program/find-sum-of-cubes-of-numbers/find_sum_of_cubes_of_numbers.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func findArraySum(arr []int) int {
+	res := 0
+	for i := 0; i < len(arr); i++ {
+		res += arr[i] * 3
+	}
+	return res
+}
+
+func main() {
+	fmt.Println(findArraySum([]int{1, 2, 3, 4, 5}))
+}
