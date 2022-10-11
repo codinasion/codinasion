a17f3fab92bde054a547384fb4ae01c2287e9f6c
Amanjeet Kumar <techamanjeet1@gmail.com>
2022-10-11 23:51:05 +0530

    done with this program in javascript

---
+++ program/convert-weeks-to-seconds/ConvertWeeksToSeconds.js
@@ -0,0 +1,15 @@
+// Write a JavaScript program to convert weeks to seconds
+
+// 1 week = 7 days = 168 hours = 10080 minutes = 604800 seconds
+
+// Input  : 5
+// Output : 3024000
+
+
+let num_of_weeks = 15;
+
+let totalSeconds = num_of_weeks * 604800;
+
+console.log(`Total Seconds in ${num_of_weeks} weeks is ${totalSeconds}`);
+
+// Contrtibuted by Technical Amanjeet 
