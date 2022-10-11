14f2754228765e6270c729ffae18e595f4f4656c
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2022-10-11 16:30:06 +0000

    add ts program for convert string to lowercase

---
+++ program/convert-string-to-lowercase/ConvertStringToLowercase.ts
@@ -0,0 +1,8 @@
+function convertToLowercase(str: string): string {
+  return str.toLowerCase();
+}
+
+const str = 'Hello World';
+console.log(`Input: ${str}`);
+const result = convertToLowercase(str);
+console.log(`Output: ${result}`);
