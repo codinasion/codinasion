32781e5e70008d57e5a07b421dc3021a68b13fe7
Osamudiame Ruth Igbinoba <ruth.igbinoba12@gmail.com>
2022-10-12 17:26:07 +0100

    Create convert-string-to-uppercase.py
    Created a python program that converts a string to uppercase

---
+++ program/convert-string-to-uppercase/convert-string-to-uppercase.py
@@ -0,0 +1,6 @@
+def convert_string_to_uppercase(string):
+    return string.upper()
+
+print(convert_string_to_uppercase("Hello World"))
+print(convert_string_to_uppercase("open source"))
+print(convert_string_to_uppercase("hacktoberfest"))
