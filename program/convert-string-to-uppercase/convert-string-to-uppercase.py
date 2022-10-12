762022910cadee5e50c06f5b5805e08dc358428d
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-12 18:49:10 -0300

    feat: allowing user's input

--- program/convert-string-to-uppercase/convert-string-to-uppercase.py
+++ program/convert-string-to-uppercase/convert-string-to-uppercase.py
@@ -1,6 +1,1 @@
-def convert_string_to_uppercase(string):
-    return string.upper()
-
-print(convert_string_to_uppercase("Hello World"))
-print(convert_string_to_uppercase("open source"))
-print(convert_string_to_uppercase("hacktoberfest"))
+print(input().upper())
