e37d49dfe005f6299fba880aac64f1dc79949e03
sidharthbh8 <sidharthbh8@gmail.com>
2022-10-13 00:46:55 +0530

    added convert minutes to hours in C++

---
+++ program/convert-minutes-to-hours/ConvertMinutesToHours.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int mins, hours;
+    cout<<"Enter Minutes"<<endl;
+    cin>>mins;
+    hours = mins/60;
+    cout<<"Minutes converted to hours\n"<<hours<<" Hours"<<endl;
+    return 0;
+}
