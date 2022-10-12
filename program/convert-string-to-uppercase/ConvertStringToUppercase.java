9d311fdeac9587549a0d42aaf084182f38dc1714
vishalpattnaik <vishalpattnaik387@gmail.com>
2022-10-12 20:54:22 +0530

    added program to convert upper case string to lower case

---
+++ program/convert-string-to-uppercase/ConvertStringToUppercase.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ConvertStringToUppercase
+{
+	public static void main(String args[])
+	{
+		String str = "Hello World";
+		str = str.toUpperCase();  //convert string to uppercase
+		System.out.println(str);
+	}
+}
