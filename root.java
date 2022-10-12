6fc947c7d2e48c21e135c5732a95c0c472c1a6b9
Sai Prakash <78696379+saiprakash255@users.noreply.github.com>
2022-10-13 00:53:28 +0530

    Added code to find the square root.

---
+++ root.java
@@ -0,0 +1,17 @@
+// LNpue Number £5 ---F? 45
+import java.util.*;
+class square{
+
+  public static void main(String[] args) {
+
+    int n;
+
+    System.out.print("Enter any Number ");
+    Scanner r = new Scanner(System.in);
+    n = r.nextInt();
+
+    double m = Math.sqrt(n);
+    System.out.print("Square root of " + n + " is " + m);
+
+  }
+}
