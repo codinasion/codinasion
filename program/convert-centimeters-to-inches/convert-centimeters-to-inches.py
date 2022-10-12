65d7617a260fa30441e6d42f1b40521c754ccf73
RC4PR4 <rcapra@aminstruments.com>
2022-10-12 10:38:25 +0200

    resolves issue #3400

--- program/convert-centimeters-to-inches/convert-centimeters-to-inches.py
+++
@@ -1,8 +0,0 @@
-#ISSUE #3412
-def centimetersToInch(centimeters):
-    return centimeters/2.54
-
-centimeters = 12.7
-print(('Input: '+str(centimeters) + " {oneOrMore}").format(oneOrMore="centimeter" if centimeters == 1 else "centimeters"))
-result = centimetersToInch(centimeters)
-print(('Output: '+str(result) + " {oneOrMore}").format(oneOrMore="inch" if result == 1 else "inches"))
