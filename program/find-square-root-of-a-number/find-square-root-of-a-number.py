93b159624e8b44e0efb4908270d1c910b1ce7d39
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-12 17:33:59 -0300

    refactor: remove function redundancy

--- program/find-square-root-of-a-number/find-square-root-of-a-number.py
+++ program/find-square-root-of-a-number/find-square-root-of-a-number.py
@@ -1,9 +1,5 @@
-import math 
+import math
 
-def squareRoot(base):
-        return  math.sqrt(base)
+base = int(input("Enter base: "))
 
-
-base = int(input("Enter base  : "))
-
-print("\nResult :", squareRoot(base))
+print("\nResult:", math.sqrt(base))
