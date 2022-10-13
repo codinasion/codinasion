66034b03f3ee9d774f51d25fe9c9d796964ecbb4
p00-x <suryapoojaryx@protonmail.com>
2022-10-13 23:20:13 +0530

    Added CheckVowel.rb

---
+++ program/check-vowel/CheckVowel.rb
@@ -0,0 +1,16 @@
+#Aurhor: suryapoojary0
+#date: "13-10-2022"
+
+puts "Input a letter in uppercase or smallcase"
+
+crux = ["a", "e" , "i", "o", "u", "A", "E", "I", "O", "U"]
+
+num = gets.strip
+
+if crux.include?(num) then
+	puts "Vowel"
+else
+	puts "Not a Vowel"
+end
+
+
