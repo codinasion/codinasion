bbbe9db4eb9f66f4537a7f4647470005b0caa1c7
Sonu Saha <sonu.saha@progress.com>
2022-10-13 14:02:36 +0530

    ISSUE-3346: Refactor to check leap year

--- program/check-leap-year/CheckLeapYear.rb
+++ program/check-leap-year/CheckLeapYear.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 def check_leap_year(year)
-  is_leap_year = false
-  if (year % 400).zero?
-    is_leap_year = true
-  elsif year % 100 != 0 && (year % 4).zero?
-    is_leap_year = true
-  end
+  is_leap_year = ((year % 400).zero? || (year % 100 != 0 && (year % 4).zero?))
   is_leap_year ? 'Leap Year' : 'Not a Leap Year'
 end
 
