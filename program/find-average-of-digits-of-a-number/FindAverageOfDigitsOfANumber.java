ac3a04d6af815d94af3826ffa543b169c8f4a979
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-13 23:34:29 -0300

    fix: remove unused imports and rev, close sc

--- program/find-average-of-digits-of-a-number/FindAverageOfDigitsOfANumber.java
+++ program/find-average-of-digits-of-a-number/FindAverageOfDigitsOfANumber.java
@@ -1,23 +1,22 @@
 import java.util.*;
-import java.io.*;
-import java.text.*;
-public class Main
-{
-    public static double AvgDigit(int n){
-        double rev=0,count=0,sum=0;
-        for(int i=n;i>0;i=i/10){
-            double mod=i%10;
+
+public class FindAverageOfDigitsOfANumber {
+    public static double AvgDigit(int n) {
+        double count = 0, sum = 0;
+        for (int i = n; i > 0; i = i / 10) {
+            double mod = i % 10;
             count++;
-            sum+=mod;
+            sum += mod;
         }
-        return sum/count;
+        return sum / count;
     }
-    
-	public static void main(String[] args) {
-	    Scanner sc=new Scanner(System.in);
-	    int n=sc.nextInt();
-		System.out.println(AvgDigit(n));
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+        System.out.println(AvgDigit(n));
+    }
 }
 
 // Contributed by Raghav Garg (@grraghav120)
