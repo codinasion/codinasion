e869b16f1dc5294e600d7d8ad8ec97d9605a4c33
devkansara <devkansara4@gmail.com>
2022-10-14 12:58:07 +0530

    Added code for finding area of cube in GoLang

--- program/find-area-of-a-cube/find_area_of_a_cube.go
+++ program/find-area-of-a-cube/find_area_of_a_cube.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var num int
-	fmt.Print("Enter the Number to find Cube = ")
+	fmt.Print("Enter the side of a cube to find its area = ")
 	fmt.Scanln(&num)
-	cube := num * num * num
-	fmt.Println("\nCube of", num, "=", cube)
+	cube := 6 * num * num
+	fmt.Println("\nArea of Cube with side", num, "=", cube)
 }
