d0a5d79bb95f69ba358b38cd829cca30303805a1
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-13 23:18:39 -0300

    fix: close Scanner object

--- program/find-volume-of-a-cone/FindVolumeOfACone.java
+++ program/find-volume-of-a-cone/FindVolumeOfACone.java
@@ -2,29 +2,31 @@ import java.util.Scanner;
 
 public class FindVolumeOfACone {
     public static void main(String[] args) {
-		double radius, height, volume = 0;
-		Scanner sc = new Scanner(System.in);
+        double radius, height, volume = 0;
+        Scanner sc = new Scanner(System.in);
 
-		do {
-			System.out.println("Radius :");
-			while (!sc.hasNextDouble()) {
-				System.out.println("Oops! That's not a number! \nRadius :");
-				sc.next();
-			}
-			radius = sc.nextDouble();
-		} while (radius <= 0.0d);
+        do {
+            System.out.print("Radius: ");
+            while (!sc.hasNextDouble()) {
+                System.out.print("Oops! That's not a number! \nRadius: ");
+                sc.next();
+            }
+            radius = sc.nextDouble();
+        } while (radius <= 0.0d);
 
-		do {
-			System.out.println("Height :");
-			while (!sc.hasNextDouble()) {
-				System.out.println("Oops! That's not a number! \nHeight :");
-				sc.next();
-			}
-			height = sc.nextDouble();
-		} while (height <= 0.0d);
+        do {
+            System.out.print("Height: ");
+            while (!sc.hasNextDouble()) {
+                System.out.print("Oops! That's not a number! \nHeight: ");
+                sc.next();
+            }
+            height = sc.nextDouble();
+        } while (height <= 0.0d);
 
-		volume = Math.PI * Math.pow(radius, 2) * (height / 3);
+        sc.close();
 
-		System.out.printf("\nVolume : %f", volume);
+        volume = Math.PI * Math.pow(radius, 2) * (height / 3);
+
+        System.out.printf("\nVolume : %f", volume);
     }
 }
