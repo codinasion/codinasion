3f81f25c9b8b7b56a31168b7e9468b22b8ecbcf3
Rick Castaneda :) <ch142568@alumno.udb.edu.sv>
2022-10-14 13:22:56 -0600

    Update script's logic

--- program/convert-decimal-to-hexadecimal/ConvertDecimalToHexadecimal.js
+++ program/convert-decimal-to-hexadecimal/ConvertDecimalToHexadecimal.js
@@ -1,17 +1,18 @@
 function convertToHex(decimalNumber) {
-    let quotient
-    let remaindersSequence = []
-    let remainder = decimalNumber / 16
-    remaindersSequence.push(remainder)
-  
-    do {
-      quotient = decimalNumber - remainder
-      remainder = quotient / 16
-      remaindersSequence.push(remainder)
-    } while (remainder !== 0)
-  
-    console.log(remaindersSequence.reverse())
-  }
-  
-  convertToHex(110);
-  
+  let remaindersSequence = [];
+  let remainder = decimalNumber % 16;
+  let quotient = Math.floor(decimalNumber / 16);
+  remaindersSequence.push(remainder);
+
+  do {
+    remainder = quotient % 16;
+    quotient = Math.floor(quotient / 16);
+
+    if (remainder > 0)
+      remaindersSequence.push(remainder);
+  } while (remainder !== 0);
+
+  return remaindersSequence.reverse().join(' ');
+}
+
+console.log(convertToHex(1500));
