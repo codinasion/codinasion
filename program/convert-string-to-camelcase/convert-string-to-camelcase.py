74c81475f35ce0e8149dcec426999280b0e28651
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-13 23:22:02 -0300

    fix: rename file

--- program/convert-string-to-camelcase/convert-string-to-camelcase.py
+++ program/convert-string-to-camelcase/convert-string-to-camelcase.py
@@ -1,7 +1,7 @@
 from re import sub
-a=input()
+
+a = input()
 # the below line removes _ or - with " ", then each word's first letter is made capital,
 # and finally we proceed with replacing " " with ""
-a=sub("r[_|-]"," ",a).title().replace(" ","")
-print(''.join([a[0].lower(),a[1:]]))
-
+a = sub("r[_|-]", " ", a).title().replace(" ", "")
+print("".join([a[0].lower(), a[1:]]))
