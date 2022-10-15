b5bd32751620cd9dfb4b6d5f3fc29b6e98621646
Greeshma <100756636+Greeshma-frontier@users.noreply.github.com>
2022-10-15 13:04:48 +0530

    Create ConvertDaysToHours.ts

---
+++ program/convert-days-to-hours/ConvertDaysToHours.ts
@@ -0,0 +1,10 @@
+function convertDaysToHours(days: number) {
+    // 1 days = 24hours
+    // 5 days = ? hours
+    // 5 days = 5 * 24 = 120 hours
+    
+    let hours = days * 24;
+    console.log(hours);
+}
+
+convertDaysToHours(5);
