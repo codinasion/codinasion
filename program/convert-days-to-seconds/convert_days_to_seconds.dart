97114be4ab89b3f7e9f22a215ef56a7b5818788a
Emmanuel B. Giwa <iamtundegiwa@gmail.com>
2022-10-15 04:00:39 +0100

    Delete convert_days_to_seconds.dart

--- program/convert-days-to-seconds/convert_days_to_seconds.dart
+++
@@ -1,9 +0,0 @@
-void main() {
-  int seconds = convertDaysToSeconds(5);
-}
-
-int convertDaysToSeconds(int days) {
-  int hours = 24, minutes = 60, seconds = 60, secondsInADay;
-  secondsInADay = hours * minutes * seconds;
-  return (days * secondsInADay).round();
-}
