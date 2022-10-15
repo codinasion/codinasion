2cb6c95cae7f20c6fc5cbc6366532b85591f161a
auleki <kodagiwa@gmail.com>
2022-10-15 07:03:17 +0100

    add convert days to seconds

--- program/convert-days-to-seconds/convert_days_to_seconds.dart
+++ program/convert-days-to-seconds/convert_days_to_seconds.dart
@@ -1,7 +1,3 @@
-void main() {
-  print(convertDaysToSeconds(5));
-}
-
 int convertDaysToSeconds(int days) {
   int hours = 24, minutes = 60, seconds = 60, secondsInADay;
   secondsInADay = hours * minutes * seconds;
