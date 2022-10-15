13a7f7b06cd4a34e5fbcbe4a3105e0f44887090b
RohithBoppey <rohithboppey1298@gmail.com>
2022-10-15 21:52:34 +0530

    added APSumC++

--- program/find-square-of-a-number/FindSquareOfANumber.cpp
+++ program/find-square-of-a-number/FindSquareOfANumber.cpp
@@ -1,30 +1,14 @@
 #include <iostream>
 #include <math.h>
-#include <vector>
-
 using namespace std;
 
-int findArithmeticProgressionSum(vector<int> v) {
+int main()
+{
+    int n  ;
+    cout<<"Enter a no. : "<<endl; 
+    cin>>n ;
 
-  int n = v.size();
+    cout<<"Square of "<<n<<" is "<<pow(n,2) ;
 
-  if (n == 1) {
-    return v[0];
-  }
-
-  int a = v[0];
-  int d = v[1] - a;
-
-  int an = a + (n - 1) * d;
-
-  return n / 2 * (a + an);
-}
-
-int main() {
-
-  vector<int> v = {-2, 4, 10, 16, 22};
-
-  cout << findArithmeticProgressionSum(v);
-
-  return 0;
+    return 0;
 }
