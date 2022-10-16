2a5604da047d62d108eb8f0c810be4e9f5fbbeed
sayak basak <91783288+mon842@users.noreply.github.com>
2022-10-16 21:08:16 +0530

    java code Convert Hours To Minutes

---
+++ program/convert-hours-to-minutes/ConvertHoursToMinutes.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * ConvertHoursToMinutes
+ */
+public class ConvertHoursToMinutes {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Input: ");
+        int x=sc.nextInt();
+        System.out.println("Output: "+(x*60));
+    }
+}
