393bb331105cb36d23e913b0e7153e3ab1d28e2f
forsigg <al.berhmiller@gmail.com>
2022-10-16 19:47:33 +0600

    add func is_lower_case (Python)

---
+++ program/check-lower-case/check-lower-case.py
@@ -0,0 +1,12 @@
+
+
+def is_lower_case(char: str) -> bool:
+    if char.lower() == char:
+        return True
+    else:
+        return False
+
+
+char_inp = input()
+if is_lower_case(char_inp):
+    print("Lower Case")
