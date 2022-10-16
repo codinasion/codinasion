e436d2cc40614a4c6aa1d7f8a30c51f96bcccb66
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-16 16:15:44 -0300

    fix: rename file

--- program/check-integer-or-float/CheckIntegerOrFloat.java
+++ program/check-integer-or-float/CheckIntegerOrFloat.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class IntOrFloat {
+public class CheckIntegerOrFloat {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
