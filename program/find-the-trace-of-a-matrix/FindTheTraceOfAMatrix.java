6c5bf98772a6ac7a3b79572e2e68f2534ad64a17
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-20 16:34:11 -0300

    feat: close scanner object, square matrix constrain
    Trace is only defined for square matrix, so I replace `rows` and `columns` with a `dimension` variable.

--- program/find-the-trace-of-a-matrix/FindTheTraceOfAMatrix.java
+++ program/find-the-trace-of-a-matrix/FindTheTraceOfAMatrix.java
@@ -5,16 +5,15 @@ public class FindTheTraceOfAMatrix {
 
   public static void main(String[] args) {
 
-    int i = 0, j, rows, columns, trace = 0;
+    int i = 0, j, dimension, trace = 0;
     scanner_input = new Scanner(System.in);
-    System.out.print("Enter Matrix Rows and Columns:  ");
-    rows = scanner_input.nextInt();
-    columns = scanner_input.nextInt();
-    int[][] matrix_array = new int[rows][columns];
+    System.out.print("Enter Matrix Dimension:  ");
+    dimension = scanner_input.nextInt();
+    int[][] matrix_array = new int[dimension][dimension];
     System.out.println("Enter the Matrix Items:  ");
-    while (i < rows) {
+    while (i < dimension) {
       j = 0;
-      while (j < columns) {
+      while (j < dimension) {
         matrix_array[i][j] = scanner_input.nextInt();
         j++;
       }
@@ -22,9 +21,9 @@ public class FindTheTraceOfAMatrix {
     }
 
     i = 0;
-    while (i < rows) {
+    while (i < dimension) {
       j = 0;
-      while (j < columns) {
+      while (j < dimension) {
         if (i == j) {
           trace = trace + matrix_array[i][j];
         }
@@ -32,7 +31,7 @@ public class FindTheTraceOfAMatrix {
       }
       i++;
     }
-
+    scanner_input.close();
     System.out.println("\nThe Trace Of the Matrix is :  " + trace);
 
   }
