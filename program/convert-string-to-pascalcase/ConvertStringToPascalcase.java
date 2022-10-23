13062fb87d3211c050dc7a531ab69cd7487aef23
bhatti <bhattiarbazparis@gmail.com>
2022-10-23 18:03:09 +0200

    :sparkles: add ConvertStringToPascalcase/java

---
+++ program/convert-string-to-pascalcase/ConvertStringToPascalcase.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ConvertStringToPascalcase {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("saisir le mot ou la phrase : ");
+        String a = sc.nextLine();
+        System.out.println("Résultat : " + toTitleCase(a));
+    }
+
+    public static String toTitleCase(String input) {
+        StringBuilder titleCase = new StringBuilder(input.length());
+        boolean nextTitleCase = true;
+
+        for (char c : input.toCharArray()) {
+            if (Character.isSpaceChar(c)) {
+                nextTitleCase = true;
+            } else if (nextTitleCase) {
+                c = Character.toTitleCase(c);
+                nextTitleCase = false;
+            }
+
+            titleCase.append(c);
+        }
+
+        return titleCase.toString();
+    }
+
+}
