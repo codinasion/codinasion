54edff59a19be589e3f4ae20e02f3cd66e966a99
Tanishq Singh <tanishqsingh640@gmail.com>
2022-10-23 17:59:01 +0530

    :truck: fix: typos

--- program/find-surface-area-of-a-cone/FindSurfaceAreaOfACone.ts
+++ program/find-surface-area-of-a-cone/FindSurfaceAreaOfACone.ts
@@ -1,5 +1,5 @@
 /**
- * @name This program finds the surface area of code
+ * @name This program finds the surface area of cone
  * @param {number} radius
  * @param {number} slantHeight 
  * @returns {number}
@@ -22,5 +22,5 @@ function FindSurfaceAreaOfACone(radius: number, slantHeight: number): number {
 console.log("Area of cone of radius 2 and slant-height 3: ");
 console.log("That is " + FindSurfaceAreaOfACone(2, 3) + " // 31.41592653589793");
 
-console.log("Numer of weeks in 3024000 seconds: ");
+console.log("Area of cone of radius 5 and slant-height 10: ");
 console.log("That is " + FindSurfaceAreaOfACone(5, 10) + " // 235.61944901923448")
