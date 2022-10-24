0568aba53adcffb58c6046dea41f3b2f8fb97aa6
Ricardo Castaneda <CH142568@alumno.udb.edu.sv>
2022-10-24 14:51:53 +0000

    Add new class to convert string to constantcase

---
+++ program/convert-string-to-constantcase/ConvertStringToConstantcase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ConvertStringToConstantcase
+{
+	public static void main(String[] args) {
+	    
+        Scanner sc = new Scanner(System.in);
+	    String a = sc.nextLine();
+        System.out.print("Input: ");
+	    String b=a.replace(' ','_');
+	    System.out.println(b.toUpperCase());
+        
+        sc.close();
+	}
+}
