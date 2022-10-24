7df41b86feb72eeb47c02179e0a445da0787d2b3
Ricardo Castaneda <CH142568@alumno.udb.edu.sv>
2022-10-24 14:54:22 +0000

    Apply format to java code

--- program/convert-string-to-constantcase/ConvertStringToConstantcase.java
+++ program/convert-string-to-constantcase/ConvertStringToConstantcase.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-public class ConvertStringToConstantcase
-{
-	public static void main(String[] args) {
-	    
+public class ConvertStringToConstantcase {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-	    String a = sc.nextLine();
+        String a = sc.nextLine();
         System.out.print("Input: ");
-	    String b=a.replace(' ','_');
-	    System.out.println(b.toUpperCase());
-        
+        String b = a.replace(' ', '_');
+        System.out.println(b.toUpperCase());
+
         sc.close();
-	}
+    }
 }
