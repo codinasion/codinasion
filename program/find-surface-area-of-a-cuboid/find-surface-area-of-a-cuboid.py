6d7595c662b74bf331c240448a65219a99aa427f
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-25 12:03:02 -0300

    fix: remove test case and typo in function name

--- program/find-surface-area-of-a-cuboid/find-surface-area-of-a-cuboid.py
+++ program/find-surface-area-of-a-cuboid/find-surface-area-of-a-cuboid.py
@@ -1,32 +1,16 @@
-# Python program to find the
-# Surface area
-# prism
- 
 import math
 
 length = float(input("Enter the length of the cuboid: "))
 breadth = float(input("Enter the breadth of the cuboid: "))
 height = float(input("Enter the height of the cuboid: "))
- 
-# function to calculate
-# Surface area
-def find_surafce_area(length, breadth, height):
-     
-    # formula of surface_area = 2(lb + bh + hl)
-    Surface_area = 2 * ( length * breadth + breadth * height + height * length)
-     
+
+# Function to calculate surface area of a cuboid
+def find_surface_area(length, breadth, height):
+
+    # Formula of surface_area = 2(lb + bh + hl)
+    surface_area = 2 * ( length * breadth + breadth * height + height * length)
+
     # Display surface area
-    print("The surface area of the cuboid is :", Surface_area)
+    print("\nThe surface area of the cuboid is: ", surface_area)
 
-find_surafce_area(length, breadth, height)
-
-
-# Test case 1
-"""
-Length : 2
-Width  : 3
-Height : 4
-
-Surface area : 52
-"""
-find_surafce_area(length=2, breadth=3, height=4)
+find_surface_area(length, breadth, height)
