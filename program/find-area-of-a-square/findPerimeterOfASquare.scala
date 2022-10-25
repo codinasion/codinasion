d251d2f533775289dca5bda46b9db6616d17c45c
Mahlangu <nzundamahlangu@gmail.com>
2022-10-25 12:25:33 -0500

    add find perimeter of a square in scala

---
+++ program/find-area-of-a-square/findPerimeterOfASquare.scala
@@ -0,0 +1,20 @@
+package com.labs1904.hwe.exercises
+
+object findPerimeterOfASquare {
+  def  findPerimeterOfASquare(side: Double) =
+    4 * side
+
+  println(findPerimeterOfASquare(2.5)) // 10
+
+  var input1: Int = 12
+  var expected1: Int = 48
+  val actual1 = findPerimeterOfASquare(input1)
+
+  assert(actual1 == expected1)
+
+  val input2: Double = 4.8
+  val expected2 = 19.2
+  val actual2 = findPerimeterOfASquare(input2)
+
+  assert(actual2 == expected2)
+}
