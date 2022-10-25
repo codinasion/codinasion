728e0782c18bf11e4f05ede4b26e6d13a9fd666e
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-10-25 16:21:05 +0200

    Convert String To SnakeCase

---
+++ program/convert-string-to-snakecase/ConvertStringToSnakecase.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class StringToSnakecase
+{
+	public static void main(String []args)
+	{
+        Scanner sc = new Scanner(System.in);
+        String x = "";
+        x+=sc.nextLine();
+        sc.close();
+        
+        StringBuilder string = new StringBuilder(x);
+        
+        for (int i = 0; i < string.length(); ++i) {
+            if (string.charAt(i) == ' ' && i < string.length() - 1)
+                string.setCharAt(i,'_');
+            }
+        System.out.println(string);
+    }
+}
