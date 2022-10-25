c518c0924a96cf5dd40689f160ed405d53b2bfe3
auleki <kodagiwa@gmail.com>
2022-10-25 09:04:50 +0100

    add main function

--- program/convert-binary-to-decimal/convert_binary_to_decimal.dart
+++ program/convert-binary-to-decimal/convert_binary_to_decimal.dart
@@ -1,3 +1,7 @@
+void main() {
+  print(convertBinaryToDecimal(1010));
+}
+
 int convertBinaryToDecimal(int binary) {
   int binaryNumber = int.parse(binary.toString(), radix: 2);
   return binaryNumber;
