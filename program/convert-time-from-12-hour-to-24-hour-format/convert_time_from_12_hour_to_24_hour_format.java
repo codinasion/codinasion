11636a44e9b1204376a90e7bc2745e7bb2539d78
Mux199 <73139732+Mux199@users.noreply.github.com>
2022-10-26 21:40:46 +0200

    Modification de l'issue 12h to 24h

--- program/convert-time-from-12-hour-to-24-hour-format/convert_time_from_12_hour_to_24_hour_format.java
+++ program/convert-time-from-12-hour-to-24-hour-format/convert_time_from_12_hour_to_24_hour_format.java
@@ -6,12 +6,24 @@ public class convert_time_from_12_hour_to_24_hour_format {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter hours: ");
-        String twelveHoursTime = sc.next();
+        System.out.print("Enter hours:(hh:mm:ssaa) ");
+        String input = sc.next();
+        String[] time = input.split(":");
         sc.close();
-        System.out.println("Input  : " + twelveHoursTime);
-        SimpleDateFormat df = new SimpleDateFormat("hh:mm:ssaa");
+        String twelveHoursTime = "";
         try {
+            switch(time.length) {
+                case 1:
+                    twelveHoursTime = time[0].substring(0, time[0].length()-2)+":00:00"+time[0].substring(time[0].length()-2, time[0].length());
+                    break;
+                case 2: 
+                    twelveHoursTime = time[0]+":"+time[1].substring(0, time[1].length()-2)+":00"+time[1].substring(time[1].length()-2, time[1].length());
+                    break;
+                default: 
+                    twelveHoursTime = input;
+                    break;
+            }
+            SimpleDateFormat df = new SimpleDateFormat("hh:mm:ssaa");
             Date date = df.parse(twelveHoursTime);
             System.out.println("Output : " + new SimpleDateFormat("HH:mm:ss").format(date));
         } catch (Exception e) {
