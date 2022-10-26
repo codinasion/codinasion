3c0a98c4fef0f12f1c725709652b68f8d5b66464
reinema <77593341+reinem@users.noreply.github.com>
2022-10-26 20:26:33 +0200

    convert hour 24h to 12h

--- program/convert-time-from-24-hour-to-12-hour-format/convert_time_from_24_hour_to_12_hour_format.java
+++ program/convert-time-from-24-hour-to-12-hour-format/convert_time_from_24_hour_to_12_hour_format.java
@@ -7,10 +7,22 @@ public class convert_time_from_24_hour_to_12_hour_format {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter hours: ");
-        String twentyFourHoursTime = sc.next();
+        String input = sc.next();
+        String[] time = input.split(":");
         sc.close();
-        System.out.println("Input  : " + twentyFourHoursTime);
+        String twentyFourHoursTime = "";
         try {
+            switch(time.length) {
+                case 1: 
+                    twentyFourHoursTime = time[0]+":00:00";
+                    break;
+                case 2: 
+                    twentyFourHoursTime = time[0]+":"+time[1]+":00";
+                    break;
+                default: 
+                    twentyFourHoursTime = input;
+                    break;
+            }
             SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
             Date date = df.parse(twentyFourHoursTime);
             System.out.println("Output : " + new SimpleDateFormat("hh:mm:ssaa").format(date));
