fc2ee58b3b5940f22cf695bd8d682b4b2a43f80f
mrajesh09 <mrajeshb4u@gmail.com>
2022-10-28 08:18:06 -0700

    Added a Program for finding average of numbers by recursion in kotlin

---
+++ program/find-average-of-numbers-by-recursion/FindAverageOfNumbersByRecursion.kt
@@ -0,0 +1,17 @@
+class FindAverageOfNumbersByRecursion {
+    // a is array, i is index and n is size of array
+    fun avgRec(a: IntArray, i: Int, n: Int): Double {
+        // for last element
+        if (i == n - 1) return a[i].toDouble()
+
+        // When index is 0, divide sum computed so far by n.
+        return if (i == 0) (a[i] + avgRec(a, i + 1, n)) / n else a[i] + avgRec(a, i + 1, n)
+        // Compute sum
+    }
+
+    fun main(args: Array<String>) {
+        val arr = intArrayOf(1, 2, 3, 4, 5)
+        val avgByRecursion = (avgRec(arr, 0, arr.size));
+        println("Average By Recursion = $avgByRecursion")
+    }
+}
