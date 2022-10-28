4d4cfae3644fd64486cdba7bab51592b20279675
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-28 20:52:32 -0300

    fix: close scanner object

--- program/convert-minutes-to-days/ConvertMinutesToDays.java
+++ program/convert-minutes-to-days/ConvertMinutesToDays.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 class ConvertMinutes2Days {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    System.out.println("Enter minutes: ");
+    System.out.print("Enter minutes: ");
     int minutes = input.nextInt();
+    input.close();
     int days = minutes/1440;
-    System.out.println("No of days: "+days);
+    System.out.print("No of days: " + days);
   }
 }
