ec93a5ee89b919749b6f5e34bf9497883da4b4e1
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-10-28 21:04:20 -0300

    fix: file name and allow user's input

--- program/convert-minutes-to-hours/ConvertMinutesToHours.py
+++
@@ -1,5 +0,0 @@
-def format_time(minutes):
-    hours_total = minutes // 60
-    minutes_total = minutes % 60
-    time_string = "{} hours {} minutes".format(hours_total, minutes_total)
-    return time_string
