b9cfebb7abb2ec01058dd77806f9daf24f65bacc
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-11-02 19:17:37 -0300

    refactor: initialize max_so_far as the 1st element

--- program/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm)/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm).py
+++ program/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm)/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm).py
@@ -1,15 +1,12 @@
 # Python program to find maximum contiguous subarray
 
 # Function to find the maximum contiguous subarray
-from sys import maxint
-
-
 def maxSubArraySum(a, size):
 
-	max_so_far = -maxint - 1
-	max_ending_here = 0
+	max_so_far = a[0]
+	max_ending_here = a[0]
 
-	for i in range(0, size):
+	for i in range(1, size):
 		max_ending_here = max_ending_here + a[i]
 		if (max_so_far < max_ending_here):
 			max_so_far = max_ending_here
@@ -23,6 +20,4 @@ def maxSubArraySum(a, size):
 
 a = [-2, -3, 4, -1, -2, 1, 5, -3]
 
-print "Maximum contiguous sum is", maxSubArraySum(a, len(a))
-
-
+print("Maximum contiguous sum is", maxSubArraySum(a, len(a)))
