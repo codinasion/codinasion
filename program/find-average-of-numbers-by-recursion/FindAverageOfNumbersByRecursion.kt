8bcf195273d2fab10f3362b1bce8c549c0ee9cef
mrajesh09 <mrajeshb4u@gmail.com>
2022-11-04 15:09:26 -0700

    Changes to run locally

--- program/find-average-of-numbers-by-recursion/FindAverageOfNumbersByRecursion.kt
+++ program/find-average-of-numbers-by-recursion/FindAverageOfNumbersByRecursion.kt
@@ -1,17 +1,14 @@
-class FindAverageOfNumbersByRecursion {
-    // a is array, i is index and n is size of array
-    fun avgRec(a: IntArray, i: Int, n: Int): Double {
-        // for last element
-        if (i == n - 1) return a[i].toDouble()
+// a is array, i is index and n is size of array
+fun avgRec(a: IntArray, i: Int, n: Int): Double {
+    // for last element
+    if (i == n - 1) return a[i].toDouble()
 
-        // When index is 0, divide sum computed so far by n.
-        return if (i == 0) (a[i] + avgRec(a, i + 1, n)) / n else a[i] + avgRec(a, i + 1, n)
-        // Compute sum
-    }
+    // When index is 0, divide sum computed so far by n and compute sum
+    return if (i == 0) (a[i] + avgRec(a, i + 1, n)) / n else a[i] + avgRec(a, i + 1, n)
+}
 
-    fun main(args: Array<String>) {
-        val arr = intArrayOf(1, 2, 3, 4, 5)
-        val avgByRecursion = (avgRec(arr, 0, arr.size));
-        println("Average By Recursion = $avgByRecursion")
-    }
+fun main() {
+    val arr = intArrayOf(1, 2, 3, 4, 5)
+    val avgByRecursion = (avgRec(arr, 0, arr.size));
+    println("Average By Recursion = $avgByRecursion")
 }
