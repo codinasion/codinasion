87d3ea4a54f5366f22460260467e7cc078b3fddf
Sami Lieberman <sami.lieberman@aexp.com>
2022-11-05 07:38:57 -0400

    [ 🚧 WIP ] ConvertMinutesToHours.js (#4797)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

--- program/convert-minutes-to-hours/ConvertMinutesToHours.js
+++ program/convert-minutes-to-hours/ConvertMinutesToHours.js
@@ -1,13 +1,16 @@
-// 60 Minutes = 1 Hour
+/**
+ *
+ * @name This program converts minutes to hours
+ * @param {number} minutes
+ * @returns {number} hours
+ *
+ */
 
-function ConvertMinutesToHours(minute){
-    let hours = minute/60
-    return hours;
-}
-
-// Test Console
-console.log(`${ConvertMinutesToHours(300)} Hours`);
-
-console.log(`${ConvertMinutesToHours(120)} Hours`);
-
-console.log(`${ConvertMinutesToHours(60)} Hours`);
+ function ConvertMinutesToHours(minutes) {
+    return minutes / 60;
+  }
+  
+  // Console Tests
+  console.log("How many hours is 60 minutes? " + ConvertMinutesToHours(60) + " hour");
+  console.log("How many hours is 1,440 minutes? " + ConvertMinutesToHours(1440) + " hours");
+  console.log("How many hours is 525,600 minutes? " + ConvertMinutesToHours(525600) + " hours");
