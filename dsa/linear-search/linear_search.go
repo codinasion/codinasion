8f494a7c21a14c8a3d702d58e4c889530ddee7c0
Madhu S Gowda <79962713+MadhuS-1605@users.noreply.github.com>
2022-11-05 15:52:22 +0530

    Update linear_search.go

--- dsa/linear-search/linear_search.go
+++ dsa/linear-search/linear_search.go
@@ -5,7 +5,7 @@ import "fmt"
 func linearSearch(A []int, n int, x int) {
 	for i := 0; i < n; i++ {
 		if A[i] == x {
-			fmt.Println("Number found at position : ", i+1)
+			fmt.Println("Number found at position : ", i)
 			return
 		}
 	}
