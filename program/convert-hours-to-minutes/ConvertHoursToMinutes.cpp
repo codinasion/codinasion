74dbe824cc75949c63049972fe3724d28031153f
Tanishq Singh <tanishqsingh640@gmail.com>
2022-11-07 10:53:13 +0530

    :bug: add: cpp program to `convert hrs to min` (#4638) (#4719)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>
    Closes https://github.com/codinasion/program/issues/4638

---
+++ program/convert-hours-to-minutes/ConvertHoursToMinutes.cpp
@@ -0,0 +1,16 @@
+///ConvertHoursToMinutes.cpp
+
+// Write a C++ program to convert hours to minutes
+// 1 hour = 60 minutes
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float hours;
+	
+	cin >> hours;
+    cout << (hours * 60) << '\n';
+}
+
+// Contributed by Tanishq Singh
