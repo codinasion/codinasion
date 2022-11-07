d4c3996e009b6286c15ba56da5fbe300f1cee6d0
sayak basak <91783288+mon842@users.noreply.github.com>
2022-11-07 10:56:42 +0530

    ConvertHoursToMinutes (#4662)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>
    Closes https://github.com/codinasion/program/issues/4636

--- program/convert-hours-to-minutes/ConvertHoursToMinutes.cpp
+++ program/convert-hours-to-minutes/ConvertHoursToMinutes.cpp
@@ -1,16 +1,11 @@
-///ConvertHoursToMinutes.cpp
-
-// Write a C++ program to convert hours to minutes
-// 1 hour = 60 minutes
-
 #include <iostream>
 using namespace std;
 
-int main() {
-    float hours;
-	
-	cin >> hours;
-    cout << (hours * 60) << '\n';
+int main()
+{
+    int x;
+    cout << "Input: " ;
+    cin >> x;
+    cout << "Output: " << x*60 <<endl;
+    return 0;
 }
-
-// Contributed by Tanishq Singh
