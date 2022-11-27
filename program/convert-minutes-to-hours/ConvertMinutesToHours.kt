3c55945221069be20e3e23a522255145c25bc54c
Rajesh Kumar <rajesh.kumar30@aexp.com>
2022-11-26 23:31:36 -0500

    Create ConvertMinutesToHours in kotlin #3513

--- program/convert-minutes-to-hours/ConvertMinutesToHours.kt
+++ program/convert-minutes-to-hours/ConvertMinutesToHours.kt
@@ -1,16 +1,17 @@
 import java.util.Scanner
+import kotlin.math.roundToInt
 
 fun main() {
     print("Enter minutes : ")
     println()
     var min = readLine()
 
-    println("Time in Hours is: " + ConvertMinutesToHours(min!!.toInt()))
+    println("Time in Hours is: " + ConvertMinutesToHours(min!!.toDouble()))
 
 }
 
-private fun ConvertMinutesToHours(min: Int): Int {
-    val hours = min / 60;
+private fun ConvertMinutesToHours(min: Double): Double {
+    val hours = min / 60
 
     return  hours
 }
