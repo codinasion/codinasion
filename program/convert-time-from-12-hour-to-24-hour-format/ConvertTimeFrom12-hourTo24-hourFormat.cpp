e045e06d0e8c19b83c4dc4b65b0512901abb81ab
Harsh Raj <harshraj8843@gmail.com>
2022-11-28 09:56:51 +0530

    Update ConvertTimeFrom12-hourTo24-hourFormat.cpp

--- program/convert-time-from-12-hour-to-24-hour-format/ConvertTimeFrom12-hourTo24-hourFormat.cpp
+++ program/convert-time-from-12-hour-to-24-hour-format/ConvertTimeFrom12-hourTo24-hourFormat.cpp
@@ -2,52 +2,57 @@
 // format
 #include<iostream>
 using namespace std;
- 
+
 void print24(string str)
 {
     // Get hours
     int h1 = (int)str[1] - '0';
     int h2 = (int)str[0] - '0';
     int hh = (h2 * 10 + h1 % 10);
- 
-    // If time is in "AM"
-    if (str[8] == 'A')
-    {
-        if (hh == 12)
-        {
-            cout << "00";
-            for (int i=2; i <= 7; i++)
-                cout << str[i];
-        }
-        else
-        {
-            for (int i=0; i <= 7; i++)
-                cout << str[i];
-        }
+    
+    if (hh > 12){
+        cout << "Are you sure that the time is in 12-hour format \n";
     }
- 
-    // If time is in "PM"
-    else
-    {
-        if (hh == 12)
+    else {
+        // If time is in "AM"
+        if (str[8] == 'A')
         {
-            cout << "12";
-            for (int i=2; i <= 7; i++)
-                cout << str[i];
+            if (hh == 12)
+            {
+                cout << "00";
+                for (int i=2; i <= 7; i++)
+                    cout << str[i];
+            }
+            else
+            {
+                for (int i=0; i <= 7; i++)
+                    cout << str[i];
+            }
         }
+    
+        // If time is in "PM"
         else
         {
-            hh = hh + 12;
-            cout << hh;
-            for (int i=2; i <= 7; i++)
-                cout << str[i];
+            if (hh == 12)
+            {
+                cout << "12";
+                for (int i=2; i <= 7; i++)
+                    cout << str[i];
+            }
+            else
+            {
+                hh = hh + 12;
+                cout << hh;
+                for (int i=2; i <= 7; i++)
+                    cout << str[i];
+            }
         }
     }
 }
- 
+
 int main()
 {
-   string str = "12:05:45PM";
+   string str = "13:05:45PM";
    print24(str);
    return 0;
 }
