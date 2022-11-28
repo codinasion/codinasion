4ed5b9857cb3c9147efa2c44aab6692be16cba86
Amanjeet Kumar <74159740+TechnicalAmanjeet@users.noreply.github.com>
2022-11-28 11:01:19 +0530

    [ 🚧 WIP ] done with this program in java (#2741)
    * done with this program in java
    
    * Update PrintNumbersFromNTo1.java
    
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

---
+++ program/print-numbers-from-n-to-1/PrintNumbersFromNTo1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// Write a Java program to print numbers from n to 1
+
+// Input  : 5
+// Output : 5 4 3 2 1
+
+public class PrintNumbersFromNTo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+
+    int i = num;
+		
+		while(i > 0)
+    {
+       System.out.println(i + " ");
+
+       i = i - 1;
+    }
+	
+
+	}
+
+}
+
+// contributed by Technical Amanjeet 
