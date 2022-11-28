1b75545a15b8992a6ae2dfc9edddb855f4540a3a
Harsh Raj <harshraj8843@gmail.com>
2022-11-28 08:48:06 +0530

    Update ConvertWeeksToSeconds.ts

--- program/convert-weeks-to-seconds/ConvertWeeksToSeconds.ts
+++ program/convert-weeks-to-seconds/ConvertWeeksToSeconds.ts
@@ -1,12 +1,8 @@
-
-
-// g
-
 function convertWeeksToSeconds(weeks: number) 
 {
-  let seconds =  weeks*7*24*60*60*60
+  let seconds =  weeks*7*24*60*60;
 
-  return convertWeeksToSeconds
+  return seconds;
 }
 
 
