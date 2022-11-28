6f546dc7ccf99c88fa29e207c9222645c5e6aec3
Harsh Raj <harshraj8843@gmail.com>
2022-11-28 09:15:23 +0530

    Update PrintNumbersFromNTo1.java

--- program/print-numbers-from-n-to-1/PrintNumbersFromNTo1.java
+++ program/print-numbers-from-n-to-1/PrintNumbersFromNTo1.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 // Input  : 5
 // Output : 5 4 3 2 1
 
-public class PrintNumbersFromNTo1() {
+public class PrintNumbersFromNTo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
