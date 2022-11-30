a4dc8050ec99b624765426796834d2e02fabf8bf
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-11-30 11:04:23 -0300

    fix: Array type and code style

--- dsa/binary-search/binarySearch.scala
+++ dsa/binary-search/binarySearch.scala
@@ -1,21 +1,21 @@
 object binarySearch {
-    def search(arr: Arrays[Int], target: Int): Int = {
+    def search(arr: Array[Int], target: Int): Int = {
         var left = 0
         var right = arr.length - 1
 
         while(left<=right) {
             val mid = left + (right - left) / 2
             if(arr(mid) == target) return mid
-            else if(arr(mid) < target) left = mid + 1
-            else right = mid - 1
-        }
-        -1
+                else if(arr(mid) < target) left = mid + 1
+                    else right = mid - 1
+                    }
+        return -1
     }
     def main(args: Array[String]): Unit = {
-        var arr = Arrays(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-        var target = 5
+        var arr = Array(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+        var target = 30
         var result = search(arr, target)
         if(result == -1) println("Element not found")
-        else println("Element found at index " + result)
-    }
+            else println("Element found at index " + result)
+            }
 }
