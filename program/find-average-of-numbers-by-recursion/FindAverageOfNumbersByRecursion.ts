09775aad736482886e9f8e15663f236873f20d14
Rakesh Tirumala <tirumalarakesh@gmail.com>
2022-12-07 19:58:28 +0530

    Create FindAverageOfNumbersByRecursion.ts

---
+++ program/find-average-of-numbers-by-recursion/FindAverageOfNumbersByRecursion.ts
@@ -0,0 +1,12 @@
+function average_of_numbers(arr: any, length: number, i: number): any {
+    if (i == length - 1) {
+      return arr[i];
+    }
+    if (i == 0) return (arr[i] + average_of_numbers(arr, length, i + 1)) / length;
+  
+    return arr[i] + average_of_numbers(arr, length, i + 1);
+  }
+  
+  let array = [1, 2, 3, 4, 5];
+  let i = 0;
+  console.log(average_of_numbers(array, array.length, i));
