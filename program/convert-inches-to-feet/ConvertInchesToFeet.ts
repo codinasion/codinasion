9540e71c10ab44acdff4511db6a044c878aca8e7
Varun Koranne <88835482+Varun-Dhruv@users.noreply.github.com>
2022-12-11 10:55:48 +0530

    Rounded of the result for converting inches to feet

--- program/convert-inches-to-feet/ConvertInchesToFeet.ts
+++ program/convert-inches-to-feet/ConvertInchesToFeet.ts
@@ -1,4 +1,5 @@
 const ConvertInchesToFeet = (Inches: number): number => {
-    return Inches / 12;
+    let result: number = parseFloat((Inches / 12).toFixed(2));
+    return result;
 }
 export default ConvertInchesToFeet;
