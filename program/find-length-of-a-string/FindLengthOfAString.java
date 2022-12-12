324945b855ff8439455f523ed8fe09ef0db2bec2
Dinesh Kumar Lalam <dineshlalam15@gmail.com>
2022-12-12 09:52:18 +0530

    Update FindLengthOfAString.java

--- program/find-length-of-a-string/FindLengthOfAString.java
+++ program/find-length-of-a-string/FindLengthOfAString.java
@@ -4,7 +4,7 @@ public class FindLengthOfAString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the String: ");
-        String s = sc.next();
+        String s = sc.nextLine();
         System.out.println("Length of the String: " +s.length());
     }
 }
