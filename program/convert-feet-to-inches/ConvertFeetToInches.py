766b3641c7848d733dba05c6538e19066b6597f7
arunkumarkota <arunkumarkota@gmail.com>
2022-12-13 13:38:49 +0530

    Add ConvertFeetToInches in Python

---
+++ program/convert-feet-to-inches/ConvertFeetToInches.py
@@ -0,0 +1,4 @@
+input_value = int(input("Please Enter The Number In Feet/Foot To Be Converted in Inches : "))
+modifier = 12
+value_in_feet = input_value * modifier
+print(f"{input_value} Feet/Foot is {value_in_feet} Inches")
