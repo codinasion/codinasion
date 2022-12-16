beb8cc9b707074c157dfc21a94392f8170086a44
Ana <56302061+anafvana@users.noreply.github.com>
2022-12-16 12:27:55 +0100

    Haskell solution: splitting string into char array

---
+++ program/split-string-into-array-of-characters/split_string_into_array_of_characters.hs
@@ -0,0 +1,11 @@
+-- String is aliased to [Char], thus no operation is needed to split a string into an array of chars
+
+-- However, the expected output is ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d"]. 
+-- Double quotes would indicate this is an array of strings. This operation can be performed as follows:
+
+-- String into array of single-character strings
+characters :: [Char] -> [[Char]]
+characters [] = []
+characters (x:xs)
+    | null xs = [[x]]
+    | otherwise = [x]:characters xs
