4b5fec9eb864aee6e251ac52251e731e9f7acff2
Harsh Raj <harshraj8843@gmail.com>
2022-12-17 20:47:23 +0530

    Update rotateMatrix.js
    Initial matrix:
    1 1 1 
    2 2 2 
    3 3 3 
    
    Rotated matrix:
    3 2 1 
    3 2 1 
    3 2 1

--- program/cylindrically-rotate-a-matrix-by-90-degrees/rotateMatrix.js
+++ program/cylindrically-rotate-a-matrix-by-90-degrees/rotateMatrix.js
@@ -1,14 +1,29 @@
-'use strict';
+"use strict";
 
 let N = 3;
 
 function rotate90Clockwise(arr) {
-  for (j = 0; j < N; j++) {
-    for (i = N - 1; i >= 0; i--)
-      console.log(arr[i][j] + " ");
+  for (let j = 0; j < N; j++) {
+    for (let i = N - 1; i >= 0; i--) {
+      process.stdout.write(arr[i][j] + " ");
+    }
+    console.log();
   }
 }
 
+let arr = [
+  [1, 1, 1],
+  [2, 2, 2],
+  [3, 3, 3],
+];
 
-let arr = [[1, 1, 1], [2, 2, 2], [3, 3, 3]];
+console.log("Initial matrix:");
+for (let i = 0; i < N; i++) {
+  for (let j = 0; j < N; j++) {
+    process.stdout.write(arr[i][j] + " ");
+  }
+  console.log();
+}
+
+console.log("\nRotated matrix:");
 rotate90Clockwise(arr);
