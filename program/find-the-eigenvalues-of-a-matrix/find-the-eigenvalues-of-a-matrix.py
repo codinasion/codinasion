ef827e6d41924606467a4f8a8d8669c594160417
Anandha Krishnan S <anandajith@gmail.com>
2022-12-18 17:42:52 +0530

    Eigen values of a matrix

---
+++ program/find-the-eigenvalues-of-a-matrix/find-the-eigenvalues-of-a-matrix.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+arr = np.array([[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]])
+  
+w, v = np.linalg.eig(arr)
+  
+print("Eigen values of the given square array:\n", w)
