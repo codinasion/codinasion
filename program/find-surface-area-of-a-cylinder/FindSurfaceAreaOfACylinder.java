dc1fde736f36ba8260927b98b0591499edd651fc
Poonam Joshi <Poonam.Joshi1@aexp.com>
2022-12-19 19:33:10 +0530

    Added program for surface area of a cyllinder

---
+++ program/find-surface-area-of-a-cylinder/FindSurfaceAreaOfACylinder.java
@@ -0,0 +1,17 @@
+class TotalSurfaceAreaOfCylinder {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter the radius:");
+
+        double radius = s.nextDouble();
+
+        System.out.println("Enter the height:");
+
+        double height = s.nextDouble();
+
+        double surfaceArea = ((2 * 22 * radius) / 7) * (radius + height);
+
+        System.out.println("TotalSurfaceArea Of Cylinder is:  " + surfaceArea);
+    }
+}
