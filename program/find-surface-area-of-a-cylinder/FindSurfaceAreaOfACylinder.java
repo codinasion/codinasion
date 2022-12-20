5f0ca0cb0c17f8bb3df39565b50db27bdd4639cf
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2022-12-20 11:19:20 -0300

    fix: using Math.PI instead of 22/7

--- program/find-surface-area-of-a-cylinder/FindSurfaceAreaOfACylinder.java
+++ program/find-surface-area-of-a-cylinder/FindSurfaceAreaOfACylinder.java
@@ -4,16 +4,14 @@ class TotalSurfaceAreaOfCylinder {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter the radius:");
-
+        System.out.print("Enter the radius: ");
         double radius = s.nextDouble();
 
-        System.out.println("Enter the height:");
-
+        System.out.print("Enter the height: ");
         double height = s.nextDouble();
 
-        double surfaceArea = ((2 * 22 * radius) / 7) * (radius + height);
+        double surfaceArea = 2 * Math.PI * radius * (radius + height);
 
-        System.out.println("TotalSurfaceArea Of Cylinder is:  " + surfaceArea);
+        System.out.println("The total surface area of the cylinder is: " + surfaceArea);
     }
 }
