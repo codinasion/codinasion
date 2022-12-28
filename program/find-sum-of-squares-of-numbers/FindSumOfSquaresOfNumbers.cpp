0934280d826193ec4f437b0ea8f8fe479ace6698
Kartikeya <kudlu2004@gmail.com>
2022-12-28 14:15:23 +0530

    Added Six CPP files to solve multiple issues

---
+++ program/find-sum-of-squares-of-numbers/FindSumOfSquaresOfNumbers.cpp
@@ -0,0 +1,17 @@
+// Calculates the sum of square of the inputted numbers.
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+ll SumOfSquares(int a[]){
+    
+}
+int main(){
+    vector<int> vec;
+    
+    return 0;
+}
+
+/*
+Contributed by : Kartikeya Saini
+*/
