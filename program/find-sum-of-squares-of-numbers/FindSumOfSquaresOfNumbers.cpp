d5664b9303bf10f89061008f9737de8aa79f0923
Kartikeya <kudlu2004@gmail.com>
2022-12-28 14:44:28 +0530

    modified file : Find sum of square of numbers Reference: PR #5811

--- program/find-sum-of-squares-of-numbers/FindSumOfSquaresOfNumbers.cpp
+++ program/find-sum-of-squares-of-numbers/FindSumOfSquaresOfNumbers.cpp
@@ -3,12 +3,16 @@
 #define ll long long
 using namespace std;
 
-ll SumOfSquares(int a[]){
-    
-}
 int main(){
-    vector<int> vec;
-    
+    ll n;   cin>>n;
+    ll sum=0;
+    for(int i=0;i<n;i++){
+        ll input;   cin>>input;
+        sum+= input*input;
+    }
+
+    cout<<sum<<endl;
+
     return 0;
 }
 
