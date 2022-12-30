78d6235251c53c7cf7dd76c174d7b36ddc7cadd8
Kartikeya <kudlu2004@gmail.com>
2022-12-30 23:27:06 +0530

    Added Print numbers from 1 to N cpp file

---
+++ program/print-numbers-from-1-to-n/PrintNumbersFrom1ToN.cpp
@@ -0,0 +1,16 @@
+// This programme prints numbers form 1 to N
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main(){
+    ll n;   cin>>n;
+    for(int i=1;i<n+1;i++){
+        cout<<i<<" ";
+    }
+    return 0;
+}
+
+/*
+Contributed by : Kartikeya Saini
+*/
