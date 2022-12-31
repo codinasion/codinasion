433ff60a0d13f45f74c0abd63fc5b87ef98f0b8a
machinedlens <76886937+mrsaxman29@users.noreply.github.com>
2022-12-31 13:23:59 -0500

    Update convert-string-to-consonantcase.py
    Create convert to consontantCase in python

--- program/convert-string-to-consonantcase/convert-string-to-consonantcase.py
+++ program/convert-string-to-consonantcase/convert-string-to-consonantcase.py
@@ -1,1 +1,15 @@
-#test
+def convert_string_to_consonantcase(str):
+  solution = ''
+  for letter in str:
+    if letter not in ['a','e','i','o','u']:
+      solution += letter.upper()
+     else:
+      solution += letter
+      
+  return solution
+
+##example
+
+print(convert_string_to_consonantcase("hello world"))
+
+## "HeLLo WoRLD"
