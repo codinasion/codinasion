c60cb997c7e2b4da02debc982051f14be6666af2
Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>
2023-01-07 01:11:41 +0530

    Corrected the formula, Fixed the typo
    There was a typo in line number 13, also the formula was wrong.

--- program/find-sum-of-gp-series/find-sum-of-gp-series.py
+++ program/find-sum-of-gp-series/find-sum-of-gp-series.py
@@ -2,15 +2,15 @@
 
 def findSumOfGpSeries(firstNumber,ratio,length):
 
-    power = pow(length,ratio)
-    ans = (firstNumber*(power - 1)/(length-1))
+    power = pow(ratio,length) 
+    ans = (firstNumber*(power - 1)/(ratio-1))
 
     return ans
 
 
 if __name__ == "__main__":
     firstNumber = float(input("give first number:\n"))
-    ratio = float(input("give ration:\n"))
+    ratio = float(input("give ratio:\n"))
     length = float(input("give length:\n"))
-
+    print("The sum of the given gp series is ")
     print(findSumOfGpSeries(firstNumber,ratio,length))
