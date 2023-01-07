844922de1c674f4c636b1eaf8329e584815719ee
Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>
2023-01-07 23:13:48 +0530

    Enabled user data entry over hard coded string
    Co-authored-by: Anandha Krishnan S <anandajith@gmail.com>

--- program/find-length-of-a-string/FindLengthOfAString.cs
+++ program/find-length-of-a-string/FindLengthOfAString.cs
@@ -1,4 +1,23 @@
-using System;
+//Find the length of the inputted string in C#
 
-string characters = "abcd";
-Console.WriteLine(characters.Length);
+using System;  
+public class Exercise2
+{  
+    public static void Main() 
+{
+    string str; /* Declares a string of size 100 */
+    int l= 0;
+	
+
+      Console.Write("Input the string : ");
+      str = Console.ReadLine();
+
+         foreach(char chr in str)
+        {
+            l += 1;
+
+        }
+
+   Console.Write("Length of the string is : {0}\n\n", l);
+	}
+}
