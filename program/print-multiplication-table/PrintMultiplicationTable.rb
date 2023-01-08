d136c40ed364c707b1f11a69e8ed81ec9597366a
Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>
2023-01-08 14:47:48 +0530

    Enabled User input instead of hard-coded number.

--- program/print-multiplication-table/PrintMultiplicationTable.rb
+++ program/print-multiplication-table/PrintMultiplicationTable.rb
@@ -1,8 +1,12 @@
+# Multiplication table in ruby...
+
+puts "Enter the number whose multiples must be shown : "
+user_input = gets
 def printMultiplicationTable number
     numArray = (1..10).to_a
     numArray.each do |num|
         puts "#{number} x #{num} = #{num*number}"
     end
-end
 
-printMultiplicationTable 2
+end
+printMultiplicationTable Integer (user_input)
