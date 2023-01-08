2a57db6c02a35de57d1a064c96eddc6c2c324f45
Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>
2023-01-08 10:56:54 +0530

    Enabled user input instead of hard-coded string
    Enabled user input...
    
    Co-authored-by: John Finley <johnfinleybremerton@gmail.com>

--- program/print-reverse-of-a-string/PrintReverseOfAString.rb
+++ program/print-reverse-of-a-string/PrintReverseOfAString.rb
@@ -1,5 +1,10 @@
+# Program to reverse the inputted string...
 def printReverseOfAString string
     puts string.reverse
 end
 
-printReverseOfAString "abcd"
+print "Enter the string : "
+string_input = gets
+
+print "The reversed string is : "
+printReverseOfAString string_input
