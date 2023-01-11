ea20c8bf3366d6b842e49eedba5b3b052aed3835
Harsh Raj <harshraj8843@gmail.com>
2023-01-11 21:48:32 +0530

    Update ConversationToBinAndDec.java

--- program/convert-decimal-to-binary/ConversationToBinAndDec.java
+++ program/convert-decimal-to-binary/ConversationToBinAndDec.java
@@ -1,4 +1,3 @@
-
 import java.util.AbstractMap.SimpleEntry;
 
 import java.util.Map;
@@ -11,27 +10,27 @@ import java.util.stream.Stream;
  */
 public class ConversationToBinAndDec {
 
-	/**
-	 * @param args the command line arguments
-	 */
-//	public static void main(String[] args) {
-//		// TODO code application logic here
-//		// System.out.println(dectoHex(16));
-//
-//	}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(dectobin(10));
 
-	public static String dectobin(int m) {
-		String s = "";
-		while (m > 0) {
-			s = String.valueOf(m % 2) + s;
-			m = m / 2;
-		}
-		return s;
-	}
+    }
 
-	
-	static Map<Integer, String> map = Stream
-			.of(new SimpleEntry<>(10, "A"), new SimpleEntry<>(11, "B"), new SimpleEntry<>(12, "C"),
-					new SimpleEntry<>(13, "D"), new SimpleEntry<>(14, "E"), new SimpleEntry<>(15, "F"))
-			.collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
+    public static String dectobin(int m) {
+        String s = "";
+        while (m > 0) {
+            s = String.valueOf(m % 2) + s;
+            m = m / 2;
+        }
+        return s;
+    }
+
+
+    static Map<Integer, String> map = Stream
+                                      .of(new SimpleEntry<>(10, "A"), new SimpleEntry<>(11, "B"), new SimpleEntry<>(12, "C"),
+                                          new SimpleEntry<>(13, "D"), new SimpleEntry<>(14, "E"), new SimpleEntry<>(15, "F"))
+                                      .collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
 }
