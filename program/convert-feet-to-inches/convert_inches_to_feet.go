49fd8fb20cee21008f0536ec2a4ad6555d7d084c
Vedant pople <vedantpople4@gmail.com>
2023-01-12 21:19:15 +0530

    solves5539

--- program/convert-feet-to-inches/convert_inches_to_feet.go
+++ program/convert-feet-to-inches/convert_inches_to_feet.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 	var inches float64
-	var feet float64
+	var feets float64
 	fmt.Scanf("%f", &inches)
-	feet = inches / 12
-	fmt.Println(feet)
+	feets = inches / 12
+	fmt.Println(feets)
 }
