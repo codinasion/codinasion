4ed3a579c3b154689e6a4dc0bd3434b3678925cf
Mohamed Moulay <69359301+Medmly20208@users.noreply.github.com>
2023-01-13 05:08:13 +0100

    convert kilometers to miles in cpp (#6008)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>
    Closes https://github.com/codinasion/program/issues/4014

---
+++ program/convert-kilometers-to-miles/ConvertKilometersToMiles.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    float miles,kilometers;
+    cout << "enter kilometers"<<endl;
+    cin>>kilometers;
+    
+    cout<<"miles "<<kilometers/1.609;
+
+    return 0;
+}
