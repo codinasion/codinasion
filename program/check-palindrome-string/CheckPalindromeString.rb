3a3fb60347b8bf8c80bb23b7bb571263a4b3cb01
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2023-01-17 11:50:34 -0300

    feat: compare lower case strings

--- program/check-palindrome-string/CheckPalindromeString.rb
+++ program/check-palindrome-string/CheckPalindromeString.rb
@@ -1,12 +1,12 @@
-puts "Please enter a string: "
+print "Please enter a string: "
 input_string = gets.chomp
 
 def checkPalindromeString string
-    if string == string.reverse
+    if string.downcase == string.reverse.downcase
         "Palindrome String"
     else
         "Not Palindrome String"
     end
 end
 
-puts checkPalindromeString input_string
+print checkPalindromeString input_string
