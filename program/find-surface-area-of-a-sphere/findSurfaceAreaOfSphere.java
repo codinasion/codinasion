c6256dfa04904bee6ccd66958adccd7e31a4b39b
Sharvil Patil <sharvilpatil13@gmail.com>
2023-01-17 20:15:40 +0530

    surface area of sphere in java

--- program/find-surface-area-of-a-sphere/findSurfaceAreaOfSphere.java
+++ program/find-surface-area-of-a-sphere/findSurfaceAreaOfSphere.java
@@ -5,6 +5,6 @@ class findSurfaceAreaOfSphere
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter radius of the sphere: ");
         double r = sc.nextDouble();
-        System.out.println("Surface Area of the Sphere: " + 4*Math.PI*r*r);
+        System.out.println("Surface Area of the Sphere with radius " + r + ": " + 4*Math.PI*r*r);
     }
 }
