a9bd63dc0808042fa10436512d48925a5fb30808
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2023-01-18 16:47:19 -0300

    feat: close scanner object

--- program/convert-fahrenheit-to-celsius/ConvertFahrenheitToCelsius.java
+++ program/convert-fahrenheit-to-celsius/ConvertFahrenheitToCelsius.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
 public class ConvertFahrenheitToCelsius {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Input ( F )  : ");
+        System.out.print("Input (ºF): ");
         int fahrenheit = sc.nextInt();
-        System.out.print("Output ( C ) : ");
-        int celcius = (fahrenheit - 32) * 5/9;
+        sc.close();
+        System.out.print("Output (ºC): ");
+        int celcius = (fahrenheit - 32) * 5 / 9;
         System.out.println(celcius);
-
     }
-
 }
 
-//can use float , double or any other data type according to convienence.
+// You can use float, double or any other data type according to convinience.
