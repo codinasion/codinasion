5735554fa618a79ed104f5cceedeb75344c44326
João Vitor <90481938+joao-vitor-souza@users.noreply.github.com>
2023-01-18 16:28:25 -0300

    feat: close scanner object

--- program/print-inverted-pyramid-pattern/PrintInvertedPyramidPattern.java
+++ program/print-inverted-pyramid-pattern/PrintInvertedPyramidPattern.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class PrintInvertedPyramidPattern {
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
-        for(int i=t;i>0;i--){
-            for(int j=1;j<i+1;j++){
-                System.out.print(j+" ");
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        sc.close();
+        for (int i = t; i > 0; i--) {
+            for (int j = 1; j < i + 1; j++) {
+                System.out.print(j + " ");
             }
             System.out.println();
         }
