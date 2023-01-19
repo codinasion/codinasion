d300ec3504370b3fdd47a779b41c24c0bac126e5
james-tharit <tharit.rt@gmail.com>
2023-01-19 09:43:46 +0700

    Convert _reverseNum logic to calculate from String

--- program/print-reverse-of-a-number-by-recursion/print_reverse_of_a_number_by_recursion.dart
+++ program/print-reverse-of-a-number-by-recursion/print_reverse_of_a_number_by_recursion.dart
@@ -1,17 +1,24 @@
-String _reverseNum({required int num, int reversed = 0}) {
+String _reverseNum(String num, {String reversed = ''}) {
+  print('next: ${num}');
   // Base, will return in last sequence, when no more digits to reverse
-  if (num == 0) return reversed.toString();
+  if (num.length == 1) return '${reversed.toString()}${num}';
 
-  int last_digit = num % 10; // Get the last digit of the number
-  int _reversed = (reversed * 10 +
-      last_digit); // Add last digit to the beginning of the reversed
-  return _reverseNum(
-      num: num ~/ 10,
+  String last_digit = num.split("").last; // Get the last digit of the number
+  String _reversed =
+      "${reversed}${last_digit}"; // Add last digit to the beginning of the reversed
+  List<String> a = num.split('');
+  a.removeLast();
+
+  String next = a.join();
+
+  return _reverseNum(next,
       reversed:
           _reversed); // Recursively call the function with remaining digits
 }
 
 void main() {
-  print(_reverseNum(num: 123)); // will print 321
-  print(_reverseNum(num: 123456789)); // will print 987654321
+  print(_reverseNum('123000')); // will print 000321
+  print(_reverseNum('123456789')); // will print 987654321
+  print(_reverseNum('0100100')); // will print 0010010
+  print(_reverseNum('0023400567')); // will print 7650043200
 }
