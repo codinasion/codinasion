33336a92c67ee366220b69c82aa7ca4aebc4cd95
Vipul Kumar <vipulkmr02@gmail.com>
2023-01-22 18:24:40 +0530

    added FindAverageOfDigitsOfANumber.c

---
+++ program/find-average-of-digits-of-a-number/FindAverageOfDigitsOfANumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+float average_of_digits(int number) {
+	int sum = 0, digits = 0;
+
+	while (number != 0) {
+		digits++;
+		sum += number % 10;
+		number /= 10;
+	}	
+
+	return (float)sum / (float)digits;
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%.0f", average_of_digits(n));
+	return 0;
+}
