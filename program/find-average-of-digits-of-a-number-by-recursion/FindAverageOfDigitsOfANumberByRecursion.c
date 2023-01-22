d4a6edd285f94c4d742cbd66926a7685d4d12bd8
Vipul Kumar <vipulkmr02@gmail.com>
2023-01-22 18:24:46 +0530

    added FindAverageOfDigitsOfANumberByRecursion.c

---
+++ program/find-average-of-digits-of-a-number-by-recursion/FindAverageOfDigitsOfANumberByRecursion.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+float average_of_digits(int number, int sum, int digits) {
+	if (number == 0) 
+		return (float)sum / (float)digits;
+
+	sum += number % 10;
+	digits++;
+	return average_of_digits(number/10, sum, digits);
+}
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	printf("%.0f", average_of_digits(n, 0, 0));
+	return 0;
+}
