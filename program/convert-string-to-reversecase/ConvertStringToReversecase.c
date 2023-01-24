583dba45677c6d6992d54da06af9116063cfac84
osher160 <osher160@gmail.com>
2023-01-24 21:33:35 +0200

    added new c file

---
+++ program/convert-string-to-reversecase/ConvertStringToReversecase.c
@@ -0,0 +1,37 @@
+#include <stdio.h> /* printf */
+
+void ConvertStringToReverseCase(char *str);
+
+int main()
+{
+    char str[13] = "hello world";
+
+    ConvertStringToReverseCase(str);
+
+    printf("%s\n",str);
+
+    return 0;
+}
+
+void ConvertStringToReverseCase(char *str)
+{
+    char *curr = str;
+    
+    while(*curr != '\0')
+    {
+        /* -checks if the curr place of the string is high | low case*/
+        
+        /* -add / remove 32( the difference between lower case and high case) to match the new ascii value*/
+        if((*curr >= 'A') && (*curr <= 'Z'))
+        {
+            *curr = *curr + 32;
+        }
+
+        else if((*curr >= 'a') && (*curr <= 'z'))
+        {
+            *curr = *curr - 32;
+        }
+
+        ++curr;
+    }
+}
