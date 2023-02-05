950676e70ec8f15d3167f4b5222d9e78c00ed5cf
Shivam Kawale <ultimateengineer2002@gmail.com>
2023-02-05 20:43:26 +0530

    invoking function for given values

--- program/calculate-compound-interest/CalculateCompoundInterest.js
+++ program/calculate-compound-interest/CalculateCompoundInterest.js
@@ -1,7 +1,7 @@
-function calculateCompoundInterest(principal, rate, time, compoundingsPerYear) {
-  const compoundFactor = 1 + rate / 100 / compoundingsPerYear;
-  const numberOfCompoundings = time * compoundingsPerYear;
-  const compoundInterest =
-    principal * Math.pow(compoundFactor, numberOfCompoundings);
-  return compoundInterest - principal;
+function calculateCompoundInterest(principal, rate, time) {
+  const compoundFactor = 1 + rate / 100;
+
+  const compoundInterest = principal * Math.pow(compoundFactor, time);
+  return compoundInterest;
 }
+console.log(calculateCompoundInterest(1000, 10, 5));
