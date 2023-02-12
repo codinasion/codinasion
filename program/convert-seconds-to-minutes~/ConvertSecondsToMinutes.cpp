c3ce14e600dccb8a0c9f4455f9ac0a7b5df5f814
Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>
2023-02-12 18:04:19 +0530

    Replaced double instead of integer input
    For much precise output.

--- program/convert-seconds-to-minutes~/ConvertSecondsToMinutes.cpp
+++ program/convert-seconds-to-minutes~/ConvertSecondsToMinutes.cpp
@@ -1,5 +1,6 @@
 /*
 Author -> Abhinav Prabhat
+Edit -> Kartikeya Saini
 Date   -> 12-02-2023
 */
 
@@ -10,11 +11,11 @@ using namespace std;
 int main()
 {
 
-    int sec;
+    double sec;
     cout << "\n\nEnter in seconds -> ";
     cin >> sec;
 
-    cout << sec << " seconds in minutes is -> " << sec / 60 << endl
+    cout << sec << " seconds in minutes is -> " << sec / 60.0 << endl
          << endl;
 
     return 0;
