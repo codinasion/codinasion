6a46146575c98a0e6f1fb15d416fbbfe69377c96
ABHINAV PRABHAT <abhiprab3@gmail.com>
2023-02-12 13:15:28 +0530

    Added code to convert seconds to minutes

---
+++ program/convert-seconds-to-minutes~/ConvertSecondsToMinutes.cpp
@@ -0,0 +1,21 @@
+/*
+Author -> Abhinav Prabhat
+Date   -> 12-02-2023
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int sec;
+    cout << "\n\nEnter in seconds -> ";
+    cin >> sec;
+
+    cout << sec << " seconds in minutes is -> " << sec / 60 << endl
+         << endl;
+
+    return 0;
+}
