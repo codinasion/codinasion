4c8dec8c5654186d01a1f2e84fbc437b44d52248
RAMON EMMIEL JASMIN <ramonjasmin8@gmail.com>
2023-02-14 05:05:35 +0800

    Added the main method and invoked the function with the sample input from the issue

--- program/convert-hexadecimal-to-decimal/ConvertHexadecimalToDecimal.java
+++ program/convert-hexadecimal-to-decimal/ConvertHexadecimalToDecimal.java
@@ -5,6 +5,11 @@
  */
 
 public class ConvertHexadecimalToDecimal{
+    public static void main(String[] args) {
+        String hexadecimal = "A";
+        System.out.println("HEXADECIMAL: " + hexadecimal);
+        System.out.println("DECIMAL EQUIVALENT: " + convertHexaToDeci(hexadecimal));
+    }
     public static String convertHexaToDeci(String hexaValue){
         return Integer.toString(Integer.parseInt(hexaValue, 16));
     }
