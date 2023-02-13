3dadc88e0694f364372982abd7b41262e42c1cc2
RAMON EMMIEL JASMIN <ramonjasmin8@gmail.com>
2023-02-14 05:11:43 +0800

    Added the main method and invoked the reverseString() method with the required input

--- program/print-reverse-of-a-string-by-recursion/PrintReverseOfAStringByRecursion.java
+++ program/print-reverse-of-a-string-by-recursion/PrintReverseOfAStringByRecursion.java
@@ -1,3 +1,5 @@
+import javax.management.relation.RelationSupport;
+
 /**
  * CONTRIBUTOR: JASMIN, RAMON EMMIEL P.
  * COUNTRY: PHILIPPINES
@@ -5,6 +7,12 @@
  */
 
 public class PrintReverseOfAStringByRecursion {
+    public static void main(String[] args) {
+        String orig = "abcd";
+
+        System.out.println("ORIGINAL STRING: " + orig);
+        System.out.println("REVERSED STRING: " + reverseString(orig));
+    }
     public static String reverseString(String str){
         if (str.isEmpty()){
             return str;
