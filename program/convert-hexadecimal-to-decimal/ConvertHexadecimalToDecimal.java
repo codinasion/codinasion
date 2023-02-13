07c6668ad483196c637a0257cc981bf8d88264b4
RAMON EMMIEL JASMIN <ramonjasmin8@gmail.com>
2023-02-13 20:24:22 +0800

    Added ConvertHexadecimalToDecimal.java class

---
+++ program/convert-hexadecimal-to-decimal/ConvertHexadecimalToDecimal.java
@@ -0,0 +1,11 @@
+/**
+ * CONTRIBUTOR: JASMIN, RAMON EMMIEL P.
+ * COUNTRY: PHILIPPINES
+ * DATE: FEBRUARY 13, 2022
+ */
+
+public class ConvertHexadecimalToDecimal{
+    public static String convertHexaToDeci(String hexaValue){
+        return Integer.toString(Integer.parseInt(hexaValue, 16));
+    }
+}
