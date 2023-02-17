0afc780e2f72c4929c4f3e1bbef5f26eb66791e3
Aranarora <aranarora007@gmail.com>
2023-02-16 18:17:42 -0800

    Update convertastringtointigerwithoutbuiltinfunction.java

--- program/convert-a-string-to-an-integer-(without-using-built-in-functions)/convertastringtointigerwithoutbuiltinfunction.java
+++ program/convert-a-string-to-an-integer-(without-using-built-in-functions)/convertastringtointigerwithoutbuiltinfunction.java
@@ -1,11 +1,13 @@
-
+import java.util.Scanner;
 public class convertastringtointigerwithoutbuiltinfunction {
     public static void main(String[] args) {
-        String str = "123";
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
         int result = 0;
         for (int i = 0; i < str.length(); i++) {
             result = result * 10 + (str.charAt(i) - '0');
         }
         System.out.println(result);
+        input.close();
     }
 }
