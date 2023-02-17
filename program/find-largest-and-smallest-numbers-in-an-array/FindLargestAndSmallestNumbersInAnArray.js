d48639e4732e9845ec2d389e15d9b9f9cef16027
pelegsch666 <peleg.mazuz@gmail.com>
2023-02-17 06:13:02 +0200

    js file findlargestandsmallestnumbers was fixed

--- program/find-largest-and-smallest-numbers-in-an-array/FindLargestAndSmallestNumbersInAnArray.js
+++ program/find-largest-and-smallest-numbers-in-an-array/FindLargestAndSmallestNumbersInAnArray.js
@@ -1,10 +1,6 @@
-function largeSmall(numbers) {
-    let sorted = numbers.slice().sort(function (a, b) {
-        return a - b;
-    });
-    return sorted;
-}
-
-let numbers = [1, 2, 3, 4, 5];
-let sort = largeSmall(numbers);
-console.log(sort[sort.length - 1], sort[0]);
+function findingSmallestLargest(numArr) {
+    return [Math.max(...numArr), Math.min(...numArr)];
+  }
+  
+  const numberArrTest = [1, 2, 3, 4, 5];
+  findingSmallLarge(numberArrTest);
