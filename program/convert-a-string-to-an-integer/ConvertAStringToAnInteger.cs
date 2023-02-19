6a44978b9be27b623df902e07a66e9889fc001cb
Anandha Krishnan S <anandajith@gmail.com>
2023-02-19 16:16:06 +0530

    String to Integer

---
+++ program/convert-a-string-to-an-integer/ConvertAStringToAnInteger.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+    Console.WriteLine ("Please enter a number :");
+    string num = Console.ReadLine();
+    Console.WriteLine("String to Integer : "+ Int32.Parse(num));
+  }
+}
