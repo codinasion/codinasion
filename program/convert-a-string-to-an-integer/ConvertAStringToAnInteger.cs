6cdf9056ed8987c8bf5b09ca551cba1a0b4be5ef
Anandha Krishnan S <anandajith@gmail.com>
2023-02-19 16:17:40 +0530

    Delete ConvertAStringToAnInteger.cs

--- program/convert-a-string-to-an-integer/ConvertAStringToAnInteger.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class MainClass {
-    public static void Main (string[] args) {
-    Console.WriteLine ("Please enter a number :");
-    string num = Console.ReadLine();
-    Console.WriteLine("String to Integer : "+ Int32.Parse(num));
-  }
-}
