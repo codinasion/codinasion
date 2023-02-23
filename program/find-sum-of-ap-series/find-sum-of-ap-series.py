6e781c9652ecff57832f12eed9a5763844ca71bd
Harsh Raj <harshraj8843@gmail.com>
2023-02-23 12:55:43 +0530

    fix: rename files and format code

--- program/find-sum-of-ap-series/find-sum-of-ap-series.py
+++ program/find-sum-of-ap-series/find-sum-of-ap-series.py
@@ -1,14 +1,13 @@
 # Program to find the sum of n-terms in an AP
 
-# The input to the program are a,n,d 
+# The input to the program are a,n,d
 # a - first term
 # n - number of terms
 # d - common difference
 
-a,n,d = list(map(int,input().split(' ')))
+a, n, d = list(map(int, input().split(" ")))
 
-if n<0:
-	print("Error! Number of terms cannot be negative")
+if n < 0:
+    print("Error! Number of terms cannot be negative")
 else:
-	print((n/2)*((2*a)+((n-1)*d)))
-
+    print((n / 2) * ((2 * a) + ((n - 1) * d)))
