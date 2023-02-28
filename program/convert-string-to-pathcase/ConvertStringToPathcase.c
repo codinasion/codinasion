7bafc9fe40ffb426c5fa46e6116f52f8139449ad
osher160 <osher160@gmail.com>
2023-02-28 13:13:34 +0200

    test done

--- program/convert-string-to-pathcase/ConvertStringToPathcase.c
+++ program/convert-string-to-pathcase/ConvertStringToPathcase.c
@@ -1,3 +1,36 @@
-#include <stdio.h>
+#include <stdio.h> /* printf, scanf */
+#include <stdlib.h> /* fgets, malloc, free */
+void ConvertStringToPathcase(char *str);
 
-void ConvertStringToPathcase(char *);
+
+int main()
+{
+    char *str = NULL;
+    char *tmp = NULL;
+    int size = 0;
+
+    if(scanf("%d",&size) != 1)
+    {
+        return -1;
+    }
+
+    str = (char *)malloc(sizeof(char) * size);
+    tmp = str;
+
+    tmp = fgets(str,size,stdin);
+
+    if(tmp == NULL)
+    {
+        free(str);
+        return -1;
+    }
+
+    ConvertStringToPathcase(str);
+
+    printf("%s\n",str);
+}
+
+void ConvertStringToPathcase(char *str)
+{
+    
+}
