3788aec62d302eeab1c63c76f7c32e30f5ad569a
osher160 <osher160@gmail.com>
2023-03-02 09:41:25 +0200

    test done

--- program/find-sum-of-numbers-by-recursion/FindSumOfNumbersByRecursion.c
+++ program/find-sum-of-numbers-by-recursion/FindSumOfNumbersByRecursion.c
@@ -1,12 +1,13 @@
-#include <stdio.h>
+#include <stdio.h> /* scanf, printf */
+#include <stdlib.h> /*malloc, free*/
 
 int FindSum(int *arr,int size);
-
+void InitArr(int *arr,int size);
 int main()
 {
     int *arr = NULL;
     int size = 0;
-    printf("how many  elements you want to insert?");
+    printf("how many  elements you want to insert?\n");
     if(1 != scanf("%d",&size))
     {
         return -1;
@@ -19,9 +20,27 @@ int main()
         return -1;
     }
 
+    InitArr(arr,size);
 
+    printf("%d\n",FindSum(arr,size));
 
+    return 0;
 }
+
+void InitArr(int *arr,int size)
+{
+    int i = 0;
+    printf("please enter all of the elements:\n");
+
+    for(i = 0; i < size; ++i)
+    {
+        if(scanf("%d",arr + i)!= 1)
+        {
+            exit(1);
+        }
+    }
+}
+
 
 int FindSum(int *arr,int size)
 {
