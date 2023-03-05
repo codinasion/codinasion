717dd61068dfed6ce36ea0c6c353c4fee6124159
osher160 <osher160@gmail.com>
2023-03-05 08:41:53 +0200

    impl done

--- program/check-digit/CheckDigit.c
+++ program/check-digit/CheckDigit.c
@@ -1,13 +1,35 @@
 #include <stdio.h> /* printf, scanf */
 
-int is_Digit(int check_me);
+int IsDigit(int check_me);
 
 int main()
 {
+    int check_me = 0;
 
+    int is_good = scanf("%d",&check_me);
+
+    if(is_good != 1)
+    {
+        return -1;
+    }
+
+    is_good = IsDigit(check_me);
+
+    if(is_good)
+    {
+        printf("Digit\n");
+    }
+
+    else
+    {
+        printf("Not a digit\n");
+
+    }
+
+    return 0;
 }
 
-int is_Digit(int check_me)
+int IsDigit(int check_me)
 {
-    
+    return ((check_me >= '0') && (check_me <= '9')) ? 1 : 0;
 }
