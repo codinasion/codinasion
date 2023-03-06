6d36efe5f029f3795a0fbd17f971f2088091b5bf
Siva Kumar Edupuganti <esivakumar18@gmail.com>
2023-03-05 22:58:29 -0700

    Created swift version of find-sum-of-numbers-by-recursion

---
+++ program/find-sum-of-numbers-by-recursion/find-sum-of-numbers-by-recursion.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+print("Enter number: ")
+var number = Int(readLine()!)!
+
+print("Sum of numbers: ", sum(value: number))
+
+func sum(value: Int) -> Int{
+   var output = 0
+   if (value > 1) {
+      for j in 1...value{
+         output = output + j
+      }
+   }
+   return output
+}
