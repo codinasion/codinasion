9ae89ccbbf3a46da2c495a661e670480dc89f1be
osher160 <osher160@gmail.com>
2023-03-07 10:29:40 +0200

    stubs

---
+++ program/convert-centimeters-to-inches/ConvertCentimetersToInches.c
@@ -0,0 +1,17 @@
+#include <stdio.h> /* printf, scanf */
+
+int ConvertToInches(float cm);
+
+int main()
+{
+    float cm = 0;
+
+
+    printf("%d\n"ConvertToInches(cm));
+}
+
+
+int ConvertToInches(float cm)
+{
+
+}
