898fa2eee64a9d5d4fc1c9062222503a5792bb34
osher160 <osher160@gmail.com>
2023-03-07 10:31:37 +0200

    tests

--- program/convert-centimeters-to-inches/ConvertCentimetersToInches.c
+++ program/convert-centimeters-to-inches/ConvertCentimetersToInches.c
@@ -6,12 +6,18 @@ int main()
 {
     float cm = 0;
 
+    if(1 != scanf("%f",&cm))
+    {
+        return -1;
+    }
 
-    printf("%d\n"ConvertToInches(cm));
+    printf("%d\n",ConvertToInches(cm));
+
+    return 0;
 }
 
 
 int ConvertToInches(float cm)
 {
-
+    return (int) (cm / 2.54);
 }
