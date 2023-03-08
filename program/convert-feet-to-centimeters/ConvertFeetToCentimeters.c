35b1bd6527e70a271f2d93b8125a250f7d5c69eb
osher160 <osher160@gmail.com>
2023-03-08 16:44:47 +0200

    test ready

--- program/convert-feet-to-centimeters/ConvertFeetToCentimeters.c
+++ program/convert-feet-to-centimeters/ConvertFeetToCentimeters.c
@@ -4,7 +4,16 @@ float ConvertToCm(int feet);
 
 int main()
 {
-    
+    int feet = 0;
+
+    if(1 != scanf("%d",&feet))
+    {
+        return -1;
+    }
+
+    printf("%d\n",ConvertToCm(feet));
+
+    return 0;
 }
 
 
