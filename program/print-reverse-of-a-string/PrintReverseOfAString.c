928afb3cdd747a6f32042bd4d9e5f9c8fd11aee8
osher160 <osher160@gmail.com>
2023-03-12 11:52:33 +0200

    new file

---
+++ program/print-reverse-of-a-string/PrintReverseOfAString.c
@@ -0,0 +1,16 @@
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* size_t */
+#include <string.h> /* strlen */
+#include <unistd.h> /* getline */
+
+void PrintReverseOfAString(char *);
+
+
+int main()
+{
+    size_t size = 0;
+
+    char *str = NULL;
+
+    getline();
+}
