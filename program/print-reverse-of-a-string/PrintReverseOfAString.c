7d77401d3235db7201098460fc02125a78bfacfa
osher160 <osher160@gmail.com>
2023-03-12 11:56:09 +0200

    stubs

--- program/print-reverse-of-a-string/PrintReverseOfAString.c
+++ program/print-reverse-of-a-string/PrintReverseOfAString.c
@@ -1,5 +1,7 @@
+#define _POSIX_C_SOURCE 200809L /* for getline*/
+
 #include <stdio.h> /* printf */
-#include <stdlib.h> /* size_t */
+#include <stdlib.h> /* size_t, free */
 #include <string.h> /* strlen */
 #include <unistd.h> /* getline */
 
@@ -12,5 +14,21 @@ int main()
 
     char *str = NULL;
 
-    getline();
+    printf("Enter the size of the arr\n");
+
+    scanf("%d",&size);
+
+    getline(&str,&size,stdin);
+
+    PrintReverseOfAString(str);
+
+
+    free(str);
+
+    return 0;
+}
+
+void PrintReverseOfAString(char *str)
+{
+
 }
