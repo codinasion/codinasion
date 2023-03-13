5b224b1a76922a9cc6bfa127b0f72854e9756d37
osher160 <osher160@gmail.com>
2023-03-13 09:02:36 +0200

    test

--- program/convert-string-to-kebabcase/ConvertStringToKebabcase.cpp
+++ program/convert-string-to-kebabcase/ConvertStringToKebabcase.cpp
@@ -5,5 +5,13 @@ void ConvertToKebabcase(std::string &);
 
 int main()
 {
-    
+    std::string str;
+
+    std::cin >> str;
+
+    ConvertToKebabcase(str);
+
+    std::cout << str << std::endl;
+
+    return 0;
 }
