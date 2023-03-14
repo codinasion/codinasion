bad1656abde7a1d23093d544791befecf1c0b920
prathik8794 <me20b030@iittp.ac.in>
2023-03-14 20:42:53 +0530

    Added WriteAProgramToCheckAnagramString swift version

--- program/write-a-program-to-check-anagram-string/WriteAProgramToCheckAnagramString.swift
+++ program/write-a-program-to-check-anagram-string/WriteAProgramToCheckAnagramString.swift
@@ -4,7 +4,7 @@ func sortedString(str: String) -> String {
     return str2
 }
 
-func AreAnaGram(str1: String, str2: String) -> Void{
+func AreAnagram(str1: String, str2: String) -> Void{
     let str1 = sortedString(str: str1)
     let str2 = sortedString(str: str2)
     if str1 == str2 {
