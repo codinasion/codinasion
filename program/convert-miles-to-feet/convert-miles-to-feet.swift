2e42cd8eee2481a838d76137a537fe3b080b2e45
Harsh Raj <harshraj8843@gmail.com>
2023-03-15 06:47:05 +0000

    rename files

--- program/convert-miles-to-feet/convert-miles-to-feet.swift
+++
@@ -1,6 +0,0 @@
-import Swift
-import Foundation
-
-print("Enter miles: ")
-var miles = Double(readLine()!)!
-print("Feet: ", miles * 5280)
--- program/convert-miles-to-kilometers/convert-miles-to-kilometers.scala
+++
@@ -1,6 +0,0 @@
-object HelloWorld {
-	def main(args: Array[String]): Unit = {
-	  val miles = scala.io.StdIn.readDouble();
-	  println("kilometers: " + miles * 1.60934);
-	}
-}
