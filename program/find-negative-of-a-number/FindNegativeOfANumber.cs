389f8616e22030436ffe5de9b98de2842eb4c360
Anandha Krishnan S <anandajith@gmail.com>
2023-03-21 15:48:57 +0530

    Negative number

---
+++ program/find-negative-of-a-number/FindNegativeOfANumber.cs
@@ -0,0 +1,9 @@
+using System;
+
+class MainClass {
+    public static void Main (string[] args) {
+    Console.WriteLine ("Please enter a number : ");
+    double num = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine("Negative number : -"+num);
+  }
+}
