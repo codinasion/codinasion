f20717cb3ac3cac1e5301376233d063003f3ac6e
Aranarora <aranarora007@gmail.com>
2023-03-22 22:12:03 -0700

    pushing code of pound to kg and weekend Days user enjoyed till now

--- program/convert-string-to-titlecase/ConvertStringToTitlecase.java
+++ program/convert-string-to-titlecase/ConvertStringToTitlecase.java
@@ -1,18 +1,15 @@
 import java.util.Scanner;
+
 public class ConvertStringToTitlecase {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine(); //Getting user input
-
-        String[] words = input.split(" "); //Will split words based on space
-        StringBuilder sb = new StringBuilder();
+        Scanner input = new Scanner(System.in);
+        String text = input.nextLine();
+        String[] words = text.split(" ");
+        StringBuilder result = new StringBuilder();
         for (String word : words) {
-            sb.append(Character.toUpperCase(word.charAt(0)));
-            sb.append(word.substring(1).toLowerCase());
-            sb.append(" ");
+            result.append(Character.toUpperCase(word.charAt(0))).append(word.substring(1)).append(" ");
         }
-
-        String output = sb.toString().trim();
-        System.out.println(output); //output string to titel case
+        System.out.println(result.toString().trim());
+        input.close();
     }
 }
