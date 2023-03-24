99dc9bba60c0e309543a1e8a775ecf384a941955
Aranarora <aranarora007@gmail.com>
2023-03-24 00:05:50 -0700

    pounds to kg converter java code

---
+++ program/Pound-To-Kg-Converter/PoundToKgConverter.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PoundToKgConverter {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter weight in pounds: ");
+        double pounds = scnr.nextDouble();
+        double kilograms = pounds * 0.453592;
+        System.out.println(pounds + " pounds is equal to " + kilograms + " kilograms.");
+        scnr.close();
+    }
+}
