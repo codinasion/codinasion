98e74f25697b9197089f3fd033fdf7813f5ad2d6
Anandha Krishnan S <anandajith@gmail.com>
2023-03-26 20:47:38 +0530

    Swap two numbers in perl

---
+++ program/swap-two-numbers/swap_two_numbers.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+$a=10;
+$b=20;
+
+$a=$a+$b;
+$b=$a-$b;
+$a=$a-$b;
+
+print "\n The value of a and b after swap ";
+print "\n The value of a : $a \n";
+print "\n The value of b : $b \n";
