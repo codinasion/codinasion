99edfcca13cda0af94ad184bfbd5394a69a4c79e
Anandha Krishnan S <anandajith@gmail.com>
2023-04-15 11:54:53 +0530

    Update PrintInvertedPyramidPattern

--- program/print-inverted-pyramid-pattern/PrintInvertedPyramidPattern.cs
+++ program/print-inverted-pyramid-pattern/PrintInvertedPyramidPattern.cs
@@ -1,29 +1,21 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace InvertedPyramidStarPatternProgram {
-  class InvertedPyramidStarPattern {
-    void Show(int input) {
-      int temp = 1;
-      for (int i = input; i >= 1; i--) {
-        for (int j = 1; j < temp; j++) {
-          Console.Write(" ");
+namespace Pattern
+{
+    public class InvertedPyramid
+    {
+        public static void Main()
+        {
+            Console.Write("Enter Number of Rows :");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            for (int i = rows; i >= 1; i--)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
-        for (int k = 1; k <= 2 * i - 1; k++) {
-          Console.Write("*");
-        }
-        temp++;
-        Console.WriteLine();
-      }
     }
-    static void Main(string[] args) {
-      InvertedPyramidStarPattern p = new InvertedPyramidStarPattern();
-      Console.WriteLine("Enter the number of rows : ");
-      int input = Convert.ToInt32(Console.ReadLine());
-      p.Show(input);
-      Console.ReadLine();
-    }
-  }
 }
