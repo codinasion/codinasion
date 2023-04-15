c8e3c2309d345b250a297e761a3b59020d833420
Anandha Krishnan S <anandajith@gmail.com>
2023-04-15 11:32:06 +0530

    update compond interest

--- program/calculate-compound-interest/CalculateCompoundInterest.ts
+++ program/calculate-compound-interest/CalculateCompoundInterest.ts
@@ -1,7 +1,7 @@
-function calculateCompoundInterest(principal: Number, rate: Number, time: Number): Number {
-  const compoundFactor: Number = 1 + rate / 100;
+function calculateCompoundInterest(principal: number, rate: number, time: number): number {
+  const compoundFactor: number = 1 + rate / 100;
 
-  const compoundInterest: Number = principal * Math.pow(compoundFactor, time);
+  const compoundInterest: number = principal * Math.pow(compoundFactor, time);
   return compoundInterest;
 }
 console.log(calculateCompoundInterest(1000, 10, 5));
