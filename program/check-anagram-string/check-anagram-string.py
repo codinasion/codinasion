d92aca2602d3a41c27927847ac9a43ba3a7e447b
micies <mici05271@gmail.com>
2023-04-17 12:19:52 +0300

    anagram string in python fixed bugs function

--- program/check-anagram-string/check-anagram-string.py
+++ program/check-anagram-string/check-anagram-string.py
@@ -1,11 +1,21 @@
 # Function to check if string is anagram or not
-def isanagram(str1,str2):
-    return sorted(str1)== sorted(str2):
+def is_anagram(str1: str, str2: str) -> bool:
+    """
+    This function to check if string is anagram or not
+    Args:
+        str1: str  The first string entered by the user
+        str2: str  The second string entered by the user
 
+    Returns:
+        True if the 2 strings contain the same words and False if not
 
-string1 = input()
-string2 = input()
-if isanagram(string1,string2) == True
-    print("Anagram Strings")
-else:
-    print("Not Anagram Strings")
+    """
+    return sorted(str1)== sorted(str2)
+
+if __name__ == '__main__':
+    string1 = input("write the first string: ")
+    string2 = input("write the second string: ")
+    if is_anagram(string1,string2):
+        print("Anagram Strings")
+    else:
+        print("Not Anagram Strings")
