af37b6ea8bb198580365c1f9bbed6d1ffab2e733
mohammed Iqbal <sahilshad1137@gmail.com>
2023-04-18 16:59:06 +0530

    Update find_the_eigenvalues_of_a_matrix_using_scala.scala

--- program/find-the-eigenvalues-of-a-matrix/find_the_eigenvalues_of_a_matrix_using_scala.scala
+++ program/find-the-eigenvalues-of-a-matrix/find_the_eigenvalues_of_a_matrix_using_scala.scala
@@ -1,16 +1,13 @@
-import org.apache.commons.math3.linear.{EigenDecomposition, RealMatrix, MatrixUtils}
+import org.apache.commons.math3.linear.{EigenDecomposition, Array2DRowRealMatrix}
 
-object EigenvaluesCalculator {
-  def main(args: Array[String]): Unit = {
-    
-    val matrixData = Array(Array(1.0, 2.0), Array(3.0, 4.0))
-    val matrix: RealMatrix = MatrixUtils.createRealMatrix(matrixData)
+val matrix = new Array2DRowRealMatrix(Array(
+  Array(1.0, 2.0, 3.0),
+  Array(4.0, 5.0, 6.0),
+  Array(7.0, 8.0, 9.0)
+))
 
-    val eigenDecomposition = new EigenDecomposition(matrix)
+val eigen = new EigenDecomposition(matrix)
+val eigenvalues = eigen.getRealEigenvalues
 
-    val eigenvalues = eigenDecomposition.getRealEigenvalues()
-
-    println("Eigenvalues:")
-    eigenvalues.foreach(println)
-  }
-}
+println("Eigenvalues:")
+eigenvalues.foreach(println)
