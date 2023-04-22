68e93639cafe801403ad4a9f088dbd24c951d58f
Anandha Krishnan S <anandajith@gmail.com>
2023-04-22 23:41:19 +0530

    Update seconds to minutes

--- program/convert-seconds-to-minutes/convert_seconds_to_minutes.dart
+++ program/convert-seconds-to-minutes/convert_seconds_to_minutes.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main(){
   print("Enter the seconds :");
-  var seconds =int.parse(stdin.readLineSync());
-  var minutes = seconds/60;
+  int seconds = int.parse(stdin.readLineSync()!);
+  int minutes = seconds~/60;
   print('Equivalent minutes : $minutes');
 }
