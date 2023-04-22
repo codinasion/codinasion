044e34c5f737813d4ad7c5ac4b138efc2e1ccba8
Anandha Krishnan S <anandajith@gmail.com>
2023-04-22 23:46:39 +0530

    Update area of a circle

--- program/find-area-of-a-circle/find_area_of_a_circle.dart
+++ program/find-area-of-a-circle/find_area_of_a_circle.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main(){
   print("Enter the radius :");
-  var radius = double.parse(stdin.readLineSync());
-  var area = 3.14 * radius * radius;
+  double radius = double.parse(stdin.readLineSync()!);
+  double area = 3.14 * radius * radius;
   print('Area of circle : $area');
 }
