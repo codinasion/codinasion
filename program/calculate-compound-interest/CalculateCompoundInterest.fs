28eb285b311b30bc29a52befd4dc8f2b1b3fdafd
Anandha Krishnan S <anandajith@gmail.com>
2023-04-23 23:35:04 +0530

    Update compound interest

--- program/calculate-compound-interest/CalculateCompoundInterest.fs
+++ program/calculate-compound-interest/CalculateCompoundInterest.fs
@@ -1,6 +1,6 @@
-let compoundInterest principal interest time : float =
-  principal * (1.0 + interest) / time
+let compoundInterest principal interest time : int =
+  principal * pown (1 + interest) time
 
-let ci = compoundInterest 1000.0 10.0 5.0
+let ci = compoundInterest 1000 10 5
 
-printfn "Compound interest : %f" ci
+printfn "Compound interest : %d" ci
