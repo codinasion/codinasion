8d8bcadbdf292d35d058127c008ff4f563129e2a
Jackson McAfee <jacksonmcafee1@gmail.com>
2023-04-22 23:13:26 -0400

    modified:   convert_feet_to_inches.r

--- program/convert-feet-to-inches/convert_feet_to_inches.r
+++ program/convert-feet-to-inches/convert_feet_to_inches.r
@@ -3,15 +3,16 @@
 # Meant to be run in interactive mode, can be run as non-interactive w/ addition of CLI
 
 # Define function feet_to_inches()
-# Takes parameter of feet (as int), returns inches (as int)
+# Takes parameter of feet (as double), returns inches (as int)
+# as.integer() will round result down
 feet_to_inches <- function(feet) {
-    return (feet * 12)
+    return (as.integer(feet * 12))
 }
 
-# Example usage, get user input and cast as integer
-feet <- readline(prompt="Input feet as an integer: ")
-feet <- as.integer(feet)
+# Example usage, get user input as numeric and cast as double
+feet <- readline(prompt="Input: ")
+feet <- as.double(feet)
 
-# Convert user input to inches, then print result
+# Convert user input to inches, then use cat() to print result
 inches <- feet_to_inches(feet)
-print(paste(feet, "feet is equivalent to", inches, "inches."))
+cat("Output:", inches, "\n")
