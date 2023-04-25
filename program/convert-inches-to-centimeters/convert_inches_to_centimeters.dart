edfeb4006a5c5d23211c90d496a082172402e71e
Anandha Krishnan S <anandajith@gmail.com>
2023-04-25 14:22:31 +0530

    update inches to centimeters

--- program/convert-inches-to-centimeters/convert_inches_to_centimeters.dart
+++ program/convert-inches-to-centimeters/convert_inches_to_centimeters.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 void main(){
   print("Enter the inches :");
-  var inches = double.parse(stdin.readLineSync());
+  var inches = double.parse(stdin.readLineSync()!);
   var cm = inches * 2.54 ;
   print('Equivalent Centimeter: $cm');
 }
