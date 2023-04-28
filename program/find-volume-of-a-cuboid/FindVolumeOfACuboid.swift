11f4c1a6ce8f7adaea3c1078ae95efe836f9ffe0
Harsh Raj <harshraj8843@gmail.com>
2023-04-28 09:58:29 +0530

    [CodeFactor] Apply fixes (#7421)
    Co-authored-by: codefactor-io <support@codefactor.io>
    Co-authored-by: Anandha Krishnan S <anandajith@gmail.com>

--- program/find-volume-of-a-cuboid/FindVolumeOfACuboid.swift
+++ program/find-volume-of-a-cuboid/FindVolumeOfACuboid.swift
@@ -1,9 +1,9 @@
 import Swift
 import Foundation
 
-var length = 2.0;
-var width = 3.0;
-var height = 4.0;
+var length = 2.0
+var width = 3.0
+var height = 4.0
 
-var volume = length * width * height;
+var volume = length * width * height
 print("Volume of cuboid: ", volume)
