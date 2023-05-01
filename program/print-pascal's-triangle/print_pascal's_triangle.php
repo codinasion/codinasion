69608d1379ae737360354fc9986aa886dcc7ad67
Harsh Raj <harshraj8843@gmail.com>
2023-05-01 18:41:46 +0530

    Update print_pascal's_triangle.php

--- program/print-pascal's-triangle/print_pascal's_triangle.php
+++ program/print-pascal's-triangle/print_pascal's_triangle.php
@@ -1,56 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    echo "Output :\n\n";
 
-<body>
-    <!-- Form is used to give a visual representation of what it could look like in a website -->
-    <form method="POST">
-        <input type="text" name="inputValueN" placeholder="Value">
-        <input type="submit" name="btnSubmit" value="Submit">
+    $rows = 5;
 
-        <!-- Sets the output on a new line instead of behind the button -->
-        <br>
-
-        <!-- PHP code -->
-        <?php
-        // Only excecute the php code when the button is pressed.
-        if (isset($_POST['btnSubmit'])) {
-
-            // Get the user input value and name it $rows for a more simplefied view of the code
-            $rows = $_POST['inputValueN'];
-
-            for ($i = 0; $i < $rows; $i++) {
-                $num = 1;
-
-                for ($j = $rows; $j > $i; $j--) {
-                    // Adds spacing around the triangle to get a nice visual representation
-                    echo "&nbsp;&nbsp;";
-                }
-
-                for ($k = 0; $k <= $i; $k++) {
-                    echo "$num";
-
-                    if ($k < $i) {
-                        // Adds spacing around the triangle to get a nice visual representation
-                        echo "&nbsp;&nbsp";
-                    }
-
-                    $num = $num * ($i - $k) / ($k + 1);
-                }
-
-                echo "<br>";
-            }
+    for ($i = 0; $i < $rows; $i++) {
+        $num = 1;
+        
+        for ($j = $rows; $j > $i; $j--) {
+            // Adds spacing around the triangle to get a nice visual representation
+            echo " ";
         }
 
-        ?>
+        for ($k = 0; $k <= $i; $k++) {
+            echo "$num";
 
-    </form>
-</body>
-
-</html>
+            if ($k < $i) {
+                // Adds spacing around the triangle to get a nice visual representation
+                echo " ";
+            }
+            $num = $num * ($i - $k) / ($k + 1);
+        }
+        
+        echo "\n";
+    }
+?>
