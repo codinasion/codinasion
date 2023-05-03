3ace77c74ba8dea7dd23155571a16225a1ac52ef
Aditya062003 <102402625+Aditya062003@users.noreply.github.com>
2023-05-03 10:07:57 +0530

    Delete find-the-eigenvalues-of-a-matrix.jl

--- program/find-the-eigenvalues-of-a-matrix/find-the-eigenvalues-of-a-matrix.jl
+++
@@ -1,9 +0,0 @@
-
-println("Enter the matrix (each row separated by a newline):")
-A = [parse(Float64, x) for x in split(readline(), '\n')]
-
-eigenvals = eigvals(A)
-
-for eigenval in eigenvals
-    println(eigenval)
-end
