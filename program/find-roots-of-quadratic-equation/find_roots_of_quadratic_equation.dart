062774a7a40e7743842681eb79f5de31a22ee333
Yuri Cunha <isyuricunha@duck.com>
2023-05-06 10:06:47 -0300

    Update find_roots_of_quadratic_equation.dart (#7541)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

--- program/find-roots-of-quadratic-equation/find_roots_of_quadratic_equation.dart
+++ program/find-roots-of-quadratic-equation/find_roots_of_quadratic_equation.dart
@@ -7,27 +7,27 @@ void main() {
   List<String> input = stdin.readLineSync()!.split(' ');
 
   // parse the input to doubles
-  double a = double.parse(input[0]);
-  double b = double.parse(input[1]);
-  double c = double.parse(input[2]);
+  double coefficientOfXSquare = double.parse(input[0]);
+  double coefficientOfX = double.parse(input[1]);
+  double constantTerm = double.parse(input[2]);
 
   // calculate the discriminant
-  double discriminant = pow(b, 2) - 4 * a * c;
+  double discriminant = pow(coefficientOfX, 2) - 4 * coefficientOfXSquare * constantTerm;
 
   // check if the discriminant is positive, negative, or zero
   if (discriminant > 0) {
     // two real and distinct roots
-    double root1 = (-b + sqrt(discriminant)) / (2 * a);
-    double root2 = (-b - sqrt(discriminant)) / (2 * a);
+    double root1 = (-coefficientOfX + sqrt(discriminant)) / (2 * coefficientOfXSquare);
+    double root2 = (-coefficientOfX - sqrt(discriminant)) / (2 * coefficientOfXSquare);
     print('The roots are: $root1 and $root2');
   } else if (discriminant == 0) {
     // one real and repeated root
-    double root = -b / (2 * a);
+    double root = -coefficientOfX / (2 * coefficientOfXSquare);
     print('The roots are: $root and $root');
   } else {
     // two complex roots
-    double realPart = -b / (2 * a);
-    double imaginaryPart = sqrt(-discriminant) / (2 * a);
+    double realPart = -coefficientOfX / (2 * coefficientOfXSquare);
+    double imaginaryPart = sqrt(-discriminant) / (2 * coefficientOfXSquare);
     print('The roots are: $realPart + ${imaginaryPart}i and $realPart - ${imaginaryPart}i');
   }
 }
