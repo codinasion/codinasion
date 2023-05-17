ebe8a3559a01a87a657342d2924ed97408dce0ce
neswatch <julien.linget+github@proton.me>
2023-05-17 10:06:48 +0200

    feat: add FindSumOfApSeries in C#

---
+++ program/find-sum-of-ap-series/FindSumOfApSeries.cs
@@ -0,0 +1,17 @@
+﻿namespace a;
+
+public class FindSumOfApSeries
+{
+    /// <summary>
+    /// Finds the sum of the AP serie at the n-th term
+    /// </summary>
+    /// <param name="a">First term</param>
+    /// <param name="n">Position of the term (starts at 1)</param>
+    /// <param name="d">Constant difference between terms</param>
+    /// <returns>The sum of an arithmetic sequence at the n-th term</returns>
+    public static int sumOfApSeries(int a, int n, int d)
+    {
+        int n_th_term = a + (n - 1) * d;
+        return n * ((a + n_th_term) / 2);
+    }
+}
