92044af6ffb9be8a5b2a78830a1f81d127afcbde
Julien LINGET <julien.linget@protonmail.com>
2023-05-17 13:12:40 +0200

    feat: add convertStringToDotcase with C#

---
+++ program/convert-string-to-dotcase/ConvertStringToDotcase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class ConvertStringToDotcase
+{
+    /// <summary>
+    /// Converts the given String to dot case, which replaces all the spaces by dots
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// // Usage
+    /// Console.WriteLine(convertStringToDotcase("hello world"));
+    /// // Output
+    /// hello.world
+    /// </code>
+    /// </example>
+    /// <param name="s"></param>
+    /// <returns></returns>
+    public static string convertStringToDotcase(string s)
+    {
+        return s.Replace(' ', '.');
+    }
+}
