4d216655ec264fe792402509fd211e733342cfc6
Akkshay Tandon <76265879+akkshayTandon@users.noreply.github.com>
2023-05-18 15:49:38 +0530

    Update ConvertStringToPascalcase.cs

--- program/convert-string-to-pascalcase/ConvertStringToPascalcase.cs
+++ program/convert-string-to-pascalcase/ConvertStringToPascalcase.cs
@@ -2,7 +2,6 @@ using System;
 using System.Globalization;
 
 public class ConvertStringToPascalCase{
-    
     public static void Main(){
         Console.WriteLine("Enter a string to convert it to PascalCase: ");
         string demoInput = Console.ReadLine();
