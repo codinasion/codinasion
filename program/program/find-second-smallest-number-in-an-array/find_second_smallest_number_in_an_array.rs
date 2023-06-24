bd3ce00fe290093916f71592c8f0dfbe9b80733a
Vedant Pople <vedantpople4@gmail.com>
2023-06-24 04:07:54 -0500

    Solves #3283 and #3388
    * Solves #3283
    * Solves #3388
    
    * Solves find sum of ap series #3283
    * Solves a Rust program to find second smallest number in an array #3388
    ---------
    
    Co-authored-by: Kartikeya Saini <111000515+hi-Kartik2004@users.noreply.github.com>

--- program/program/find-second-smallest-number-in-an-array/find_second_smallest_number_in_an_array.rs
+++ program/program/find-second-smallest-number-in-an-array/find_second_smallest_number_in_an_array.rs
@@ -12,3 +12,4 @@ fn main(){
     }
     println!("{second_small}");
 }    
+
---
+++ program/program/find-sum-of-ap-series/find_sum_of_ap_series.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let a = 2;
+    let n = 5;
+    let d = 3;
+    let last = a + (n-1)*d;
+    let answer = (n*(a + last))/2;
+    println!("{answer}");
+}   
