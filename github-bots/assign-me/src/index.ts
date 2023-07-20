e4a4a4e10739c5a5625bff4da80de081a285f135
Thaung Than Han <ericlimmm115@gmail.com>
2023-07-20 23:32:04 +0700

    bug : exclude suggestion from assignment (#4545)
    Co-authored-by: Harsh Raj <harshraj8843pro@gmail.com>

--- github-bots/assign-me/src/index.ts
+++ github-bots/assign-me/src/index.ts
@@ -6,40 +6,45 @@ export = (app: Probot) => {
     // if comment body is "!assign" or "!assign-me" or "!assign me"
     const body = context.payload.comment.body;
     if (body.match(/!assign(-me)?/i)) {
-      // if issue contains label
-      // "good first issue" or "good-first-issue" or
-      // "good first issues" or "good-first-issues"
-      const labels = context.payload.issue.labels;
-      if (
-        labels.some((label) =>
-          label.name.match(/good[- ]?first[- ]?issue(s)?/i)
-        )
-      ) {
-        // if issue is not assigned to anyone
-        if (!context.payload.issue.assignee) {
-          // assign issue to the comment author
-          const assignee = context.payload.comment.user.login;
-          const assign = context.issue({ assignees: [assignee] });
-          await context.octokit.issues.addAssignees(assign);
-        }
-        // if issue is assigned to the comment author
-        else if (
-          context.payload.issue.assignee.login ===
-          context.payload.comment.user.login
+      // Check if issue creator is comment creator
+      const commentCreator = context.payload.comment.user.login;
+      const issueCreator = context.payload.issue.user.login;
+      if (issueCreator !== commentCreator) {
+        // if issue contains label
+        // "good first issue" or "good-first-issue" or
+        // "good first issues" or "good-first-issues"
+        const labels = context.payload.issue.labels;
+        if (
+          labels.some((label) =>
+            label.name.match(/good[- ]?first[- ]?issue(s)?/i)
+          )
         ) {
-          // create a comment to inform the comment author
-          const issueComment = context.issue({
-            body: `Hey @${context.payload.comment.user.login}, you are already assigned to this issue!`,
-          });
-          await context.octokit.issues.createComment(issueComment);
-        }
-        // if issue is assigned to someone else
-        else {
-          // create a comment to inform the comment author
-          const issueComment = context.issue({
-            body: `Hey @${context.payload.comment.user.login}, this issue is already assigned to @${context.payload.issue.assignee.login}!`,
-          });
-          await context.octokit.issues.createComment(issueComment);
+          // if issue is not assigned to anyone
+          if (!context.payload.issue.assignee) {
+            // assign issue to the comment author
+            const assignee = context.payload.comment.user.login;
+            const assign = context.issue({ assignees: [assignee] });
+            await context.octokit.issues.addAssignees(assign);
+          }
+          // if issue is assigned to the comment author
+          else if (
+            context.payload.issue.assignee.login ===
+            context.payload.comment.user.login
+          ) {
+            // create a comment to inform the comment author
+            const issueComment = context.issue({
+              body: `Hey @${context.payload.comment.user.login}, you are already assigned to this issue!`,
+            });
+            await context.octokit.issues.createComment(issueComment);
+          }
+          // if issue is assigned to someone else
+          else {
+            // create a comment to inform the comment author
+            const issueComment = context.issue({
+              body: `Hey @${context.payload.comment.user.login}, this issue is already assigned to @${context.payload.issue.assignee.login}!`,
+            });
+            await context.octokit.issues.createComment(issueComment);
+          }
         }
       }
     }
