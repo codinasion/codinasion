10994d5d666018d0b5b15bc20249cb1e3cc155de
Moshe Shoker <moshe@shoker.co.il>
2023-08-03 23:52:43 +0300

    find the maximum sum of a contiguous subarray (circular) (#4562)
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

--- program/program/find-the-maximum-sum-of-a-contiguous-subarray-(circular)/FindTheMaximumSumOfAContiguousSubarray(circular).ts
+++ program/program/find-the-maximum-sum-of-a-contiguous-subarray-(circular)/FindTheMaximumSumOfAContiguousSubarray(circular).ts
@@ -1,13 +1,29 @@
-function getTheHighstSumInArr(arr: []): number {
-  const arrToWork = arr.sort((a, b) => a - b);
-  [...new Set(arrToWork)];
-  const arrToSum: number[] = [];
-  let index = arr.length;
-  for (let i = 0; i < 4; i++) {
-    arrToSum.push(arrToWork[--index]);
+const maxSumSubarrayCircularTs: any = (nums: number[]) => {
+  if (nums.length === 0) return 0;
+
+  let sum = nums[0];
+
+  let currMax: number = nums[0];
+  let currMin: number = nums[0];
+  let maxTotal: number = nums[0];
+  let minTotal: number = nums[0];
+
+  for (let i = 1; i < nums.length; i++) {
+    let val: number = nums[i];
+
+    currMax = Math.max(val, currMax + val);
+    maxTotal = Math.max(currMax, maxTotal);
+
+    currMin = Math.min(val, currMin + val);
+    minTotal = Math.min(minTotal, currMin);
+
+    sum += val;
   }
-  const res = arrToSum.reduce((a, b) => {
-    return a + b;
-  });
-  return res;
-}
+
+  if (sum == minTotal) return maxTotal;
+
+  return Math.max(sum - minTotal, maxTotal);
+};
+
+var arr: number[] = [1, -2, 3, -2, 8, -5, 1, 7];
+console.log(maxSumSubarrayCircularTs(arr));
