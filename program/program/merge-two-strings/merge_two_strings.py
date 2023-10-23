ed3f4c772342511d64601fee0f9e327c0c78b96f
einanyo <63548286+einan202@users.noreply.github.com>
2023-10-23 12:27:22 +0300

    python merge two strings (#4677)
    * python merge two strings
    
    * Update merge_two_strings.py
    
    ---------
    
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

--- program/program/merge-two-strings/merge_two_strings.py
+++ program/program/merge-two-strings/merge_two_strings.py
@@ -1,8 +1,10 @@
+# This python script calculates the  concatenation of two given strings str1 and str2
+
 string1 = str(input("Enter a string  : "))
 string2 = str(input("Enter another string : "))
 
 
-def mergeString(str1, str2):
+def mergeString(str1: str, str2: str) -> str:
     return str1 + str2
 
 
