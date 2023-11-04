b14a8f4ee5f3869d0758d7bae656015dd32fc428
Harsh Raj <harshraj8843@gmail.com>
2023-11-05 00:05:09 +0530

    Rename files (#4688)

--- program/program/find-the-eigenvalues-of-a-matrix/find-the-eigenvalues-of-a-matrix.r
+++
@@ -1,5 +0,0 @@
-# Create a 3x3 matrix 
-A = matrix(c(1:9), 3, 3) 
-    
-# Calculating Eigenvalues
-print(eigen(A)$values)
--- program/program/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm)/find_the_maximum_sum_of_a_contiguous_subarray_(kadane's_algorithm).php
+++ program/program/find-the-maximum-sum-of-a-contiguous-subarray-(kadane's-algorithm)/find_the_maximum_sum_of_a_contiguous_subarray_(kadane's_algorithm).php
@@ -1,22 +1,25 @@
 <?php
-function kadaneAlgorithm($array, $size) {
-  $max = 0;
-  $current = 0;
-  for($i=0; $i<$size; $i++) {
-    $current = $current + $array[$i];
-    
-    if ($current < 0)
-      $current = 0; 
-    
-    if($max < $current)
-      $max = $current; 
-  }
-  return $max;
+
+function maxSubArraySum($a, $size)
+{
+	$max_so_far = PHP_INT_MIN;
+	$max_ending_here = 0;
+
+	for ($i = 0; $i < $size; $i++)
+	{
+		$max_ending_here = $max_ending_here + $a[$i];
+		if ($max_so_far < $max_ending_here)
+			$max_so_far = $max_ending_here;
+
+		if ($max_ending_here < 0)
+			$max_ending_here = 0;
+	}
+	return $max_so_far;
 }
 
-//test
-$array = array(1, 2, 3, -2, 5);
-$size = sizeof($array);
-$result = kadaneAlgorithm($array, $size);
-echo "Maximum SubArray is: ".$result."\n";
+$a = array(-2, -3, 4, -1, -2, 1, 5, -3);
+$n = count($a);
+$max_sum = maxSubArraySum($a, $n);
+echo "Maximum sum of contiguous subarray : " ,$max_sum;
+
 ?>
