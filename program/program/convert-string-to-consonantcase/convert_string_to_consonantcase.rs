9197b3c4423494afe7b0070194c616e79b7888ef
Swapnil Mukherjee <swapnilmukherjee24@gmail.com>
2023-12-04 08:50:16 -0500

    Commit for convert_string_to_consonantcase.rs (#4841)
    * Commit of find-sum-of-digits-of-a-number.jl
    
    * Commit of find-sum-of-digits-of-a-number.jl
    
    * Commit for print_numbers_from_1_to_n_without_using_a_loop.pl
    
    * Create workspace.xml
    
    * Delete .idea/workspace.xml
    
    * Commit for convert_string_to_consonantcase.rs
    
    ---------
    
    Co-authored-by: Harsh Raj <harshraj8843@gmail.com>

--- program/program/convert-string-to-consonantcase/convert_string_to_consonantcase.rs
+++ program/program/convert-string-to-consonantcase/convert_string_to_consonantcase.rs
@@ -1,14 +1,35 @@
-fn main(){
-    let input = "hello world!";
-    let mut ans = String::with_capacity(input.len());
-    let mut next = true;
-    for i in input.chars(){
-        if next{
-            ans.push(i.to_ascii_uppercase());
-            next = false;
-        } else {
-            ans.push(i.to_ascii_lowercase());
-        }
+use std::io;
+
+fn main() {
+    println!("Enter a string:");
+    let mut input = String::new();
+
+    // Reading input from user and handling possible errors
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        eprintln!("Failed to read line: {}", e);
+        return;
     }
-    println!("{}", ans);
+
+    // Processing the input string
+    match to_consonantcase(&input.trim()) {
+        Ok(consonantcase_string) => println!("Consonantcase: {}", consonantcase_string),
+        Err(e) => eprintln!("Error processing string: {}", e),
+    }
+}
+
+fn to_consonantcase(s: &str) -> Result<String, &'static str> {
+    // Check for empty string
+    if s.is_empty() {
+        return Err("Input string is empty");
+    }
+
+    let transformed = s.chars().map(|c| {
+        match c {
+            'a' | 'e' | 'i' | 'o' | 'u' |
+            'A' | 'E' | 'I' | 'O' | 'U' => c.to_lowercase().to_string(),
+            _ => c.to_uppercase().to_string(),
+        }
+    }).collect();
+
+    Ok(transformed)
 }
