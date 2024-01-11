3fd8e159b3b49d545595b633166557380644cf99
Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
2024-01-11 09:46:20 +0530

    Rename program files (#5188)

--- program/program/move-all-zeroes-to-end-of-array/move-all-zeroes-to-end-of-array.py
+++
@@ -1,7 +0,0 @@
-def zeroes_to_end(arr: list) -> list:
-    result = [x for x in arr if x != 0] + [x for x in arr if x == 0]
-    return result
-
-if __name__ == '__main__':
-    arr = [1, 2, 0, 4, 3, 0, 5, 0]
-    print(zeroes_to_end(arr))
--- program/program/move-all-zeroes-to-end-of-array/move_all_zeroes_to_end_of_array.py
+++ program/program/move-all-zeroes-to-end-of-array/move_all_zeroes_to_end_of_array.py
@@ -1,17 +1,7 @@
-def main():
-      """
-      Python program to move all zeroes at the end of the array  
-      """
-      
-      move_zeroes([1, 2, 0, 4, 3, 0, 5, 0])
-
-def move_zeroes(seq):
-      l = 0
-      for r in range(len(seq)):
-            if seq[r]:                                          # if non-zero value
-                  seq[l], seq[r] = seq[r], seq[l]               # swap
-                  l += 1                                          # increment l
-      print(seq)
+def zeroes_to_end(arr: list) -> list:
+    result = [x for x in arr if x != 0] + [x for x in arr if x == 0]
+    return result
 
 if __name__ == '__main__':
-      main()
+    arr = [1, 2, 0, 4, 3, 0, 5, 0]
+    print(zeroes_to_end(arr))
