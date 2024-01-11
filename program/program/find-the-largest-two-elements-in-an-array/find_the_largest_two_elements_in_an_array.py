610b958fd922400a3ca3ef85a9db1d27768eac53
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-01-11 07:12:47 +0300

    Two largest elements in array (#5161)
    * Python program: moves zeroes to end of array
    
    * Solution: find the largest two elements in an array
    
    ---------
    
    Co-authored-by: Harsh Raj <54644599+harshraj8843@users.noreply.github.com>

---
+++ program/program/find-the-largest-two-elements-in-an-array/find_the_largest_two_elements_in_an_array.py
@@ -0,0 +1,24 @@
+def main():
+      """
+            Return the two largest elements from a nonempty Python list (array)
+            1. Sort the array
+            2. Return the last two elements from the sorted array
+            
+            Asymptotic analysis
+            By sorting the sequence of the elements, elements will be placed next to each other. Therefore
+            1. built in function sorted guarantees a worst case running time of O(n log n)
+            2. return tuple assignment is in constant time O(1)
+            
+      """
+      seq = [12, 13, 1, 10, 34, 35]                                                # Test with various sequences
+      
+      largest, second_largest = find_two_largest_elements(seq)                      #unpacking tuple
+      print(f'Largest: {largest} || Second largest: {second_largest}')
+
+def find_two_largest_elements(S):
+      S_sorted = sorted(S)
+      
+      return (S_sorted[-2], S_sorted[-1])                                       # Return in tuple form
+
+if __name__ == '__main__':
+      main()
