e5516d16f596768f43904c73e704c2bf02ba4218
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-01-12 06:59:09 +0300

    Program to find the three largest elements in an array (#5190)
    Co-authored-by: Harsh Raj <54644599+harshraj8843@users.noreply.github.com>

---
+++ program/program/find-the-largest-three-elements-in-an-array/find_the_largest_three_elements_in_an_array.py
@@ -0,0 +1,24 @@
+def main():
+      seq = [10, 4, 3, 50, 23, 90, 1, 100, 49]
+      print(largest_three_elements(seq))
+
+def largest_three_elements(S):
+      """ Return the three largest elements of sequence S """
+      max1 = S[0]
+      max2 = S[0]
+      max3 = S[0]
+      
+      for val in S:
+            if val > max1:
+                  max3 = max2
+                  max2 = max1
+                  max1 = val
+            elif val > max2:
+                  max3 = max2
+                  max2 = val
+            elif val > max3:
+                  max3 = val
+      return (max1, max2, max3)
+
+if __name__ == '__main__':
+      main()
