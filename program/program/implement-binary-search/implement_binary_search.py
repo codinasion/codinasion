4b9cbd042b898921c56eca2b6537321f7a10320f
Amedi Imbira <155066628+Amedi-Imbira@users.noreply.github.com>
2024-01-13 09:10:50 +0300

    #5207 binary search implementation (#5218)
    * Program to find the three largest elements in an array
    
    * #5207 - binary search with python
    
    ---------
    
    Co-authored-by: Harsh Raj <54644599+harshraj8843@users.noreply.github.com>

---
+++ program/program/implement-binary-search/implement_binary_search.py
@@ -0,0 +1,24 @@
+def main():
+      seq = 1, 2, 3, 4, 5, 6, 7           # Test here. Seq should be sorted
+      print(binary_search(seq, 90))
+
+def binary_search(data, target):
+      """ 
+            locating a target value within a sorted sequence
+            return True if target is found
+      """
+      left = 0
+      right = len(data)-1
+      
+      while left <= right:
+            mid = (left + right) // 2
+            if target == data[mid]:                   # Found match!
+                  return True
+            elif target < data[mid]:                  # Search values left of mid
+                  right = mid-1
+            else:                                     # Search values right of mid
+                  left = mid+1 
+      return False                                    # loop ended without success
+
+if __name__ == '__main__':
+      main()
