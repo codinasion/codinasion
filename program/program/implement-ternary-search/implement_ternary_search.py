9d3c4ba74989b4442f156be44ff9d64606885200
Parvez Khan <137421089+Parvezkhan0@users.noreply.github.com>
2024-01-18 22:26:27 +0530

    Ternary search implementation (#5356)
    * Commit message for submodule changes
    
    * Implement Ternary Search algorithm in Python
    
    ---------
    
    Co-authored-by: Parvez Khan <parvezkhan0802@gmail.com>
    Co-authored-by: Ritesh Kokam <61982298+RiteshK-611@users.noreply.github.com>

---
+++ program/program/implement-ternary-search/implement_ternary_search.py
@@ -0,0 +1,33 @@
+def ternary_search(arr, value):
+    l, r = 0, len(arr) - 1
+
+    while l <= r:
+        partition_size = (r - l) // 3
+        mid1 = l + partition_size
+        mid2 = r - partition_size
+
+        if arr[mid1] == value:
+            return mid1
+
+        if arr[mid2] == value:
+            return mid2
+
+        if value < arr[mid1]:
+            r = mid1 - 1
+        elif value > arr[mid2]:
+            l = mid2 + 1
+        else:
+            l = mid1 + 1
+            r = mid2 - 1
+
+    return -1
+
+# Example usage
+arr = [1, 2, 3, 4, 5]
+value = 4
+result = ternary_search(arr, value)
+
+if result != -1:
+    print(f"Element {value} found at index {result}")
+else:
+    print(f"Element {value} not found in the array")
