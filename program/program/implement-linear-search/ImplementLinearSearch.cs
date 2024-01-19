896d044ef8dd1c8861ae72fe640a93ef571c9f50
DaniilKarpushko <112875941+DaniilKarpushko@users.noreply.github.com>
2024-01-19 11:47:04 +0300

    feat: Created file LinearSearch on C# (#5359)
    info: Closes issue #5292
    
    Co-authored-by: Harsh Raj <54644599+harshraj8843@users.noreply.github.com>

---
+++ program/program/implement-linear-search/ImplementLinearSearch.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        /*
+                Input data:
+                key
+                array size
+                array data
+            */
+        public static void Main(string[] args)
+        {
+            int key = Convert.ToInt32(Console.ReadLine());
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            
+            for(int i = 0; i < size; ++i)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int res = LinearSearch(arr, key);
+
+            if (res != -1)
+            {
+                Console.WriteLine($"Element found with index {res}");
+            }
+            else
+            {
+                Console.WriteLine("Element does not exist");
+            }
+        }
+
+        public static int LinearSearch(int[] arr, int key)
+        {
+            for(int i = 0; i < arr.Length; ++i )
+            {
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
+
