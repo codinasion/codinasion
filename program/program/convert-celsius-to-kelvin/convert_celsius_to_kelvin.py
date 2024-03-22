afd749db8aa96a58d814493c7c688f31f32ef587
magar51 <madhuri.agarwal@aexp.com>
2024-03-22 20:20:39 +0530

    New program to convert temperature from celsius to kelvin (#5515)
    * New feature to convert date from standard format to julian and vice-versa
    
    * Delete program/program/date-converter/README.md
    
    * Rename dateconverter.py to convert_standard_date_format_to_julian_date_and_vice-versa.py
    
    * New feature added which can convert temperature from celsius to kelvin
    
    * Delete program/program/convert-celsius-to-kelvin/README.md
    
    README.md will be added by the script
    
    * Delete program/program/convert-standard-date-format-to-julian-date-and-vice-versa/convert_standard_date_format_to_julian_date_and_vice-versa.py
    
    ---------
    
    Co-authored-by: Anandha Krishnan S <anandajith@gmail.com>
    Co-authored-by: Harsh Raj <54644599+harshraj8843@users.noreply.github.com>
    Co-authored-by: Yuri Cunha <isyuricunha@duck.com>

---
+++ program/program/convert-celsius-to-kelvin/convert_celsius_to_kelvin.py
@@ -0,0 +1,11 @@
+# program to convert celsius to kelvin
+
+C = input("Input (C) : ")
+
+if float(C) > -273.15:
+    K = (float(C) + 273.15)
+
+    print(f"Output (K) : {K}")
+
+else:
+    print("Error: Invalid value provided for Celsius")
