dfd3417706a9a6794fb678331ffe6485b2c80087
Harsh Raj <harshraj8843@gmail.com>
2024-03-28 12:05:04 +0530

    Rename files (#5602)

--- program/program/print-diamond-pattern/print_diamond_pattern.pl
+++ program/program/print-diamond-pattern/print_diamond_pattern.pl
@@ -1,29 +1,27 @@
-print "Enter a number :";
-my $n=<STDIN>;
-print "\n"; 
-$space = $n - 1; 
-  
-for ($i = 0; $i < $n; $i++) 
-{ 
-    for ($j = 0;$j < $space; $j++) {
-        print " "; 
-    }
-    for ($j = 0;$j <= $i; $j++) {
-        print "* "; 
-    }
-print "\n"; 
-$space--; 
-} 
-$space = 0; 
+# Perl program to print pattern 1
 
-for ($i = $n; $i > 0; $i--) 
-{ 
-    for ($j = 0; $j < $space; $j++) {
-         print " "; 
+# Function to print pattern
+sub print_pattern {
+    my $num = shift;
+
+    # Loop to iterate through each row
+    for my $i (1..$num) {
+        # Loop to print numbers in each row
+        for my $j (1..$i) {
+            print "$j ";
+        }
+        print "\n";
     }
-    for ($j = 0;$j < $i;$j++) {
-        print "* "; 
-    }
-print "\n"; 
-$space++; 
-} 
+}
+
+# Main program
+print "Enter the number of rows: ";
+my $input = <STDIN>;
+chomp($input);
+
+# Check if input is a positive integer
+if ($input =~ /^\d+$/ && $input > 0) {
+    print_pattern($input);
+} else {
+    print "Invalid input. Please enter a positive integer.\n";
+}
