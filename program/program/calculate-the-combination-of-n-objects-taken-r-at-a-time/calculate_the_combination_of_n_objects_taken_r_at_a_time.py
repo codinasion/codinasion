961de3c2a2ebc62c05e449c7f1efce86e7cb4883
Greeshmanth Penugonda <79304704+Greeshmanth1@users.noreply.github.com>
2024-05-04 02:15:04 +0530

    Greeshmanth1 patch 1 (#5674)
    * Update calculate_the_combination_of_n_objects_taken_r_at_a_time.py
    
    * Update calculate_the_combination_of_n_objects_taken_r_at_a_time.py
    
    ---------
    
    Co-authored-by: Anandha Krishnan S <anandajith@gmail.com>

--- program/program/calculate-the-combination-of-n-objects-taken-r-at-a-time/calculate_the_combination_of_n_objects_taken_r_at_a_time.py
+++ program/program/calculate-the-combination-of-n-objects-taken-r-at-a-time/calculate_the_combination_of_n_objects_taken_r_at_a_time.py
@@ -1,14 +1,16 @@
-def nCr(n, r):
-    return factorial(n) / (factorial(r) * factorial(n - r))
-
-
-def factorial(n):
-    if n == 0:
+def ncr(n, r):
+    if r > n:
+        return 0
+    if r == 0 or r == n:
         return 1
-    else:
-        return n * factorial(n - 1)
-
-
-n = 5
-r = 2
-print(int(nCr(n, r)))
+    r = min(r, n - r)  # take advantage of symmetry
+    # ncr    =    n! / (n-r)! * r!     =     n*(n-1)*(n-2)*..*(n-r+1)*(n-r)! / (n-r)! * r!     =     n*(n-1)*(n-2)*..*(n-r+1) / r!
+    numerator = 1   # (n-r)!
+    denominator = 1 # r!
+    for i in range(r):
+        numerator *= (n - i)
+        denominator *= (i + 1)
+    return numerator // denominator
+n=int(input("Enter the n value: "))
+r=int(input("Enter the r value: "))
+print("npr value is:", ncr(n,r))
