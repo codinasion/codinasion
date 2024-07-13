26b629353f7ba1386042b01187ecfcf9153609c8
Harsh Raj <harshraj8843@gmail.com>
2024-07-13 21:17:30 +0530

    update

--- sync.sh
+++ sync.sh
@@ -12,9 +12,5 @@ git checkout dev
 # Merge the latest changes from master
 git merge master
 
-# Automatically generate a commit message with the current date
-COMMIT_MESSAGE="Merge branch 'master' into dev"
-git commit -am "$COMMIT_MESSAGE"
-
 # Push the changes to the dev branch
 git push
