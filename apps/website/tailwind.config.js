35999227cd7f95bbd2b4eef12348e4bb9f3a25cf
Harsh Raj <harshraj8843@gmail.com>
2024-07-13 10:47:37 +0530

    chore: Move files to src

--- apps/website/tailwind.config.js
+++ apps/website/tailwind.config.js
@@ -3,8 +3,7 @@ import { nextui } from "@nextui-org/theme";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
