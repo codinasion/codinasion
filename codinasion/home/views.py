8a33d3fb5d49fd2cfb708ca33f7b0287ac73037f
Harsh Raj <harshraj8843@gmail.com>
2024-08-15 11:41:14 +0530

    Add home (#7703)

--- codinasion/home/views.py
+++ codinasion/home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return render(
+            request,
+            "index.html",
+        )
