9fc87011b26502fdde71292b34f5fa4fe38500fe
Harsh Raj <harshraj8843@gmail.com>
2024-08-16 10:57:52 +0530

    chore: update static file settings for Django

--- codinasion/codinasion/settings.py
+++ codinasion/codinasion/settings.py
@@ -131,9 +131,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = "/static/"
-STATIC_ROOT = ""
-STATICFILES_DIRS = ("static",)
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # Default primary key field type
