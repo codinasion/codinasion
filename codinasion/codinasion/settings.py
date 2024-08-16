0cf1cea1551edaf319a716523289ae56ca798120
Harsh Raj <harshraj8843@gmail.com>
2024-08-16 10:29:27 +0530

    Homepage (#7707)

--- codinasion/codinasion/settings.py
+++ codinasion/codinasion/settings.py
@@ -132,12 +132,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-
-# Media files (Images, Videos)
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+STATIC_ROOT = ""
+STATICFILES_DIRS = ("static",)
 
 
 # Default primary key field type
