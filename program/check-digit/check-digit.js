e9ade3225f15c3cb68822a532cb4f24f8d73072e
Harsh Raj <harshraj8843@gmail.com>
2024-08-17 21:50:08 +0530

    Remove check-consonant and check-digit programs and related files

--- program/check-digit/check-digit.js
+++
@@ -1,6 +0,0 @@
-function isNumDigit(dig) {
-  return /^[0-9]$/.test(dig) ? "Yes, digit" : "No, not a digit";
-}
-console.log(isNumDigit("1"));
-console.log(isNumDigit("w"));
-console.log(isNumDigit("6.9"));
